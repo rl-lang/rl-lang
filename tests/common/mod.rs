443d1e88e65cd0d8092ef41e9acf3e1111a2b2ef
MohamedGonem <nightangelyt01@gmail.com>
2026-06-10 08:12:56 +0300

    fix common utils

--- tests/common/mod.rs
+++ tests/common/mod.rs
@@ -1,14 +1,10 @@
-use rl_lang::{
-    ast::statements::Statement, lexer::tokenizer::Tokenizer, parser::parser_logic::Parser,
-    utils::source::SourceFile,
-};
+use rl_lang::{ast::statements::Statement, utils::source::SourceFile};
 
 pub fn lex(source: &str) -> Vec<rl_lang::lexer::tokentypes::Token> {
-    Tokenizer::lex(SourceFile::new("test", source.to_string()))?
+    let text = SourceFile::new("test", source.to_string());
+    rl_lang::logic_loops::lexing_loop(text)
 }
 
 pub fn parse(source: &str) -> Vec<Statement> {
-    let sf = SourceFile::new("test", source.to_string());
-    let tokens = Tokenizer::lex(sf.clone())?;
-    Parser::parse(tokens, sf)?
+    rl_lang::logic_loops::parsing_loop(SourceFile::new("test", source.to_string()), lex(source))
 }
