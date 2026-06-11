8bbbac5bcbddfff9eed20044217fe8253691b1ae
MohamedGonem <nightangelyt01@gmail.com>
2026-06-11 13:25:09 +0300

    add trim variants

--- src/interpreter/stdlib/string/mod.rs
+++ src/interpreter/stdlib/string/mod.rs
@@ -1,14 +1,18 @@
 mod to_lower;
 mod to_upper;
 mod trim;
+mod trim_end;
+mod trim_start;
 
 use crate::interpreter::native::Module;
 
-pub const KEYWORDS: &[&str] = &["to_lower", "to_upper"];
+pub const KEYWORDS: &[&str] = &["to_lower", "to_upper", "trim", "trim_end", "trim_start"];
 
 pub fn module() -> Module {
     Module::new("str")
         .with_function("to_upper", to_upper::std_to_upper)
         .with_function("to_lower", to_lower::std_to_lower)
         .with_function("trim", trim::std_trim)
+        .with_function("trim_end", trim_end::std_trim_end)
+        .with_function("trim_start", trim_start::std_trim_start)
 }
