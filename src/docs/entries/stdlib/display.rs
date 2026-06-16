9e1daca9f5ddce876fecf1c4a8e658ff5791fdde
MohamedGonem <73464078+MohamedGonem@users.noreply.github.com>
2026-06-16 09:11:38 +0300

    add eprint docs entry

--- src/docs/entries/stdlib/display.rs
+++ src/docs/entries/stdlib/display.rs
@@ -6,7 +6,7 @@ pub static DISPLAY: StdEntry = StdEntry {
     functions: FUNCTIONS,
 };
 
-static FUNCTIONS: &[&FnEntry] = &[&PRINT, &PRINTLN, &LEN];
+static FUNCTIONS: &[&FnEntry] = &[&PRINT, &PRINTLN, &LEN, &EPRINT];
 
 static PRINT: FnEntry = FnEntry {
     signature: "print(x)",
@@ -24,4 +24,10 @@ static LEN: FnEntry = FnEntry {
     signature: "len(x)",
     description: "length of string or array",
     example: "get std::display::len\n\nlen(\"hello\") // 5",
+};
+
+static EPRINT: FnEntry = FnEntry {
+    signature: "eprint(string)",
+    description: "halts evaluation with an error containing the given message",
+    example: "get std::display::eprint\n\neprint(\"something went wrong\") // ✗ error: something went wrong",
 };
