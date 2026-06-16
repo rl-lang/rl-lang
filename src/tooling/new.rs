6f1fceeefea3583282173b2a6f0ab17b5e0e7124
MohamedGonem <73464078+MohamedGonem@users.noreply.github.com>
2026-06-16 08:27:50 +0300

    add rl-version field and make src/ folder

--- src/tooling/new.rs
+++ src/tooling/new.rs
@@ -12,29 +12,26 @@ fn try_create_project(name: &str) -> io::Result<()> {
     let toml = format!(
         r#"[project]
 name = "{}"
+rl-version = "{}"
 version = "0.0.1"
-entry = "main.rl"
+entry = "src/main.rl"
 "#,
-        name
+        name,
+        env!("CARGO_PKG_VERSION"),
     );
-
     let main = r#"get println from std::display
-
 fn main() {
     println("hello world")
 }
-
 main()
 "#;
-
     std::fs::create_dir(name)?;
+    std::fs::create_dir(format!("{}/src", name))?;
     std::fs::write(format!("{}/rl.toml", name), toml)?;
-    std::fs::write(format!("{}/main.rl", name), main)?;
+    std::fs::write(format!("{}/src/main.rl", name), main)?;
     std::fs::write(format!("{}/.gitignore", name), "")?;
-
     std::process::Command::new("git")
         .args(["init", name])
         .output()?;
-
     Ok(())
 }
