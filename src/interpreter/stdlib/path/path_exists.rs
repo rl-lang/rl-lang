b0c09f887592fbbaec4e8aba0e43a060d261dc73
MohamedGonem <73464078+MohamedGonem@users.noreply.github.com>
2026-06-16 23:26:28 +0300

    fix reference

--- src/interpreter/stdlib/path/path_exists.rs
+++ src/interpreter/stdlib/path/path_exists.rs
@@ -5,7 +5,7 @@ use crate::{
 
 pub fn std_path_exists(_: &mut Evaluator, path: Value) -> Result<Value, Error> {
     match path {
-        Value::String(s) => Ok(Value::Bool(std::path::Path::new(s).exists())),
+        Value::String(s) => Ok(Value::Bool(std::path::Path::new(&s).exists())),
         other => Err(Error::init(
             format!("path_exists() expects a string, got {}", other.type_name()),
             None,
