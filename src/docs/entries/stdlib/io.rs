010d7d114568c8a79e17f187931e4c537322472e
MohamedGonem <73464078+MohamedGonem@users.noreply.github.com>
2026-06-17 17:13:30 +0300

    add rest of entries

--- src/docs/entries/stdlib/io.rs
+++ src/docs/entries/stdlib/io.rs
@@ -13,6 +13,11 @@ static FUNCTIONS: &[&FnEntry] = &[
     &READ_INT_PROMPT,
     &READ_FLOAT,
     &READ_FLOAT_PROMPT,
+    &APPEND_FILE,
+    &DELETE_FILE,
+    &READ_FILE,
+    &READ_LINES,
+    &WRITE_FILE,
 ];
 
 static READ: FnEntry = FnEntry {
@@ -49,4 +54,34 @@ static READ_FLOAT_PROMPT: FnEntry = FnEntry {
     signature: "read_float(prompt)",
     description: "prints prompt and reads a line from stdin then parses to float",
     example: "get std::io::read_float\n\ndec float pi = read_float(\"enter your pi: \")",
+};
+
+static APPEND_FILE: FnEntry = FnEntry {
+    signature: "append_file(path, content)",
+    description: "",
+    example: "get std::io::append_file\n\nappend_file(\"info.txt\", \"name: Mohamed\")",
+};
+
+static DELETE_FILE: FnEntry = FnEntry {
+    signature: "delete_file(path)",
+    description: "",
+    example: "get std::io::delete_file\n\ndelete_file(\"info.txt\")",
+};
+
+static READ_FILE: FnEntry = FnEntry {
+    signature: "read_file(path)",
+    description: "",
+    example: "get std::io::read_file\n\ndec string data = read_file(\"backup_info.txt\")",
+};
+
+static READ_LINES: FnEntry = FnEntry {
+    signature: "read_lines(path)",
+    description: "",
+    example: "get std::io::read_lines\n\ndec arr[string] data = read_lines(\"index.html\")",
+};
+
+static WRITE_FILE: FnEntry = FnEntry {
+    signature: "write_file(path, contents)",
+    description: "",
+    example: "get std::io::write_file\n\nwrite_file(\"index.html\", \"<p>hello \\\"Mohamed\\\"</p>\")",
 };
