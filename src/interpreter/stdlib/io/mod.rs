24de667bb53f5eabbf40a2e94b0ddf850ea32724
MohamedGonem <73464078+MohamedGonem@users.noreply.github.com>
2026-06-17 16:36:45 +0300

    add read_file()

--- src/interpreter/stdlib/io/mod.rs
+++ src/interpreter/stdlib/io/mod.rs
@@ -1,12 +1,14 @@
-pub mod input;
+mod input;
+mod read_file;
 
 use crate::interpreter::native::Module;
 
-pub const KEYWORDS: &[&str] = &["read", "read_int", "read_float"];
+pub const KEYWORDS: &[&str] = &["read", "read_int", "read_float", "read_file"];
 
 pub fn module() -> Module {
     Module::new("io")
         .with_raw_function("read", input::std_read)
         .with_raw_function("read_int", input::std_read_int)
         .with_raw_function("read_float", input::std_read_float)
+        .with_function("read_file", read_file::std_read_file)
 }
