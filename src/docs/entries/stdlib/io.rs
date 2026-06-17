be92b1ae00f78735e51fc22fd84f8f0be2fc251f
MohamedGonem <73464078+MohamedGonem@users.noreply.github.com>
2026-06-17 17:02:55 +0300

    add read_int() and read_float() entry

--- src/docs/entries/stdlib/io.rs
+++ src/docs/entries/stdlib/io.rs
@@ -6,7 +6,14 @@ pub static IO: StdEntry = StdEntry {
     functions: FUNCTIONS,
 };
 
-static FUNCTIONS: &[&FnEntry] = &[&READ, &READ_PROMPT];
+static FUNCTIONS: &[&FnEntry] = &[
+    &READ,
+    &READ_PROMPT,
+    &READ_INT,
+    &READ_INT_PROMPT,
+    &READ_FLOAT,
+    &READ_FLOAT_PROMPT,
+];
 
 static READ: FnEntry = FnEntry {
     signature: "read()",
@@ -18,4 +25,28 @@ static READ_PROMPT: FnEntry = FnEntry {
     signature: "read(prompt)",
     description: "prints prompt and reads a line from stdin",
     example: "get std::io::read\n\ndec str name = read(\"enter your name: \")",
+};
+
+static READ_INT: FnEntry = FnEntry {
+    signature: "read_int()",
+    description: "read a line from stdin then parses to integer",
+    example: "get std::io::read_int\n\ndec int age = read()",
+};
+
+static READ_INT_PROMPT: FnEntry = FnEntry {
+    signature: "read_int(prompt)",
+    description: "prints prompt and reads a line from stdin then parses to integer",
+    example: "get std::io::read_int\n\ndec int age = read_float(\"enter your age: \")",
+};
+
+static READ_FLOAT: FnEntry = FnEntry {
+    signature: "read_float()",
+    description: "read a line from stdin then parses to float",
+    example: "get std::io::read_float\n\ndec float pi = read_float()",
+};
+
+static READ_FLOAT_PROMPT: FnEntry = FnEntry {
+    signature: "read_float(prompt)",
+    description: "prints prompt and reads a line from stdin then parses to float",
+    example: "get std::io::read_float\n\ndec float pi = read_float(\"enter your pi: \")",
 };
