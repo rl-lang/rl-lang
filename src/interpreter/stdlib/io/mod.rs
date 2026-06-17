dfb730c4b52d560c7d24acfccab3b1b9a0d3ae4f
MohamedGonem <73464078+MohamedGonem@users.noreply.github.com>
2026-06-17 16:06:43 +0300

    add read_int() and read_float() to std

--- src/interpreter/stdlib/io/mod.rs
+++ src/interpreter/stdlib/io/mod.rs
@@ -2,8 +2,11 @@ pub mod input;
 
 use crate::interpreter::native::Module;
 
-pub const KEYWORDS: &[&str] = &["read"];
+pub const KEYWORDS: &[&str] = &["read", "read_int", "read_float"];
 
 pub fn module() -> Module {
-    Module::new("io").with_raw_function("input", input::std_read)
+    Module::new("io")
+        .with_raw_function("read", input::std_read)
+        .with_raw_function("read_int", input::std_read_int)
+        .with_raw_function("read_float", input::std_read_float)
 }
