9ea7640617dc7fef4cca136ea230d52437311928
MohamedGonem <73464078+MohamedGonem@users.noreply.github.com>
2026-06-19 08:58:44 +0300

    remove else if logic and embrace recusring

--- src/parser/statements/if_statement.rs
+++ src/parser/statements/if_statement.rs
@@ -10,8 +10,6 @@ impl Parser {
     pub fn parse_if(&mut self, start: Span) -> Result<Statement, Error> {
         let if_condition = self.parse_expression()?;
         let if_body = self.parse_block()?;
-        #[cfg(feature = "debug")]
-        log::debug!("parsed if branch");
         let if_branch_span = start.join(self.previous_span());
         let if_branch = Statement::new(
             StatementKind::ConditionalBranch {
@@ -20,61 +18,38 @@ impl Parser {
             },
             if_branch_span,
         );
-        let mut elseif_branch = Vec::new();
-        let mut else_body = Vec::new();
-        let mut else_start: Option<Span> = None;
-        let mut else_end_span: Span = if_branch_span;
+
         while matches!(self.peek(), TokenType::Newline) {
             self.advance();
         }
-        while self.peek() == TokenType::Else {
+
+        let else_branch = if self.peek() == TokenType::Else {
             let branch_start = self.peek_span();
             self.advance();
             if self.peek() == TokenType::If {
+                let elif_start = self.peek_span();
                 self.advance();
-                let branch_condition = self.parse_expression()?;
-                let branch_body = self.parse_block()?;
+                // Recurse and produces a nested Conditional
+                Some(Box::new(self.parse_if(elif_start)?))
+            } else {
+                let else_body = self.parse_block()?;
                 let span = branch_start.join(self.previous_span());
-                let branch = Statement::new(
+                Some(Box::new(Statement::new(
                     StatementKind::ConditionalBranch {
-                        condition: Some(branch_condition),
-                        body: branch_body,
+                        condition: None,
+                        body: else_body,
                     },
                     span,
-                );
-                #[cfg(feature = "debug")]
-                log::debug!("parsed else if branch");
-                elseif_branch.push(branch);
-                while matches!(self.peek(), TokenType::Newline) {
-                    self.advance();
-                }
-            } else {
-                else_start = Some(branch_start);
-                else_body = self.parse_block()?;
-                else_end_span = self.previous_span();
+                )))
             }
-        }
-
-        let else_branch = if else_body.is_empty() {
-            None
         } else {
-            #[cfg(feature = "debug")]
-            log::debug!("parsed else branch");
-            let span = else_start.unwrap_or(if_branch_span).join(else_end_span);
-            Some(Box::new(Statement::new(
-                StatementKind::ConditionalBranch {
-                    condition: None,
-                    body: else_body,
-                },
-                span,
-            )))
+            None
         };
 
         let span = start.join(self.previous_span());
         Ok(Statement::new(
             StatementKind::Conditional {
                 if_branch: Box::new(if_branch),
-                elseif_branch: Some(elseif_branch),
                 else_branch,
             },
             span,
