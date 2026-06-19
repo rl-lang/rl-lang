fdc67a14585982be20d172de0975c964afa6d415
MohamedGonem <73464078+MohamedGonem@users.noreply.github.com>
2026-06-19 11:32:40 +0300

    replace Error::init with err() in len()

--- src/interpreter/stdlib/len.rs
+++ src/interpreter/stdlib/len.rs
@@ -1,16 +1,18 @@
-use crate::{interpreter::evaluator::Evaluator, interpreter::values::Value, utils::errors::Error};
+use crate::{
+    interpreter::{evaluator::Evaluator, values::Value},
+    utils::{errors::Error, span::Span},
+};
 
-pub fn std_len(_: &mut Evaluator, v: Value) -> Result<i64, Error> {
+pub fn std_len(eval: &mut Evaluator, v: Value, span: Span) -> Result<i64, Error> {
     match v {
         Value::Values { items, .. } => Ok(items.len() as i64),
         Value::String(s) => Ok(s.len() as i64),
-        other => Err(Error::init(
+        other => Err(eval.err(
             format!(
                 "len() expects an array or string, got {}",
                 other.type_name()
             ),
-            None,
-            None,
+            span,
         )),
     }
 }
