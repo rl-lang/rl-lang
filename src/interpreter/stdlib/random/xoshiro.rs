f60a10adfc30c20df7d210dd56aaed941373ff84
MohamedGonem <73464078+MohamedGonem@users.noreply.github.com>
2026-06-20 22:57:12 +0300

    add next() function

--- src/interpreter/stdlib/random/xoshiro.rs
+++ src/interpreter/stdlib/random/xoshiro.rs
@@ -19,12 +19,31 @@ impl Xoshiro256 {
         let mut state = [0u64; 4];
         let mut x = seed;
         for i in 0..4 {
+            // golden ratio * 2^64
             x = x.wrapping_add(0x9e3779b97f4a7c15);
+            // magic constants
             x = (x ^ (x >> 30)).wrapping_mul(0xbf58476d1ce4e5b9);
             x = (x ^ (x >> 27)).wrapping_mul(0x94d049bb133111eb);
             state[i] = x ^ (x >> 31);
         }
 
         Self { state }
+    }
+
+    fn next(&mut self) -> u64 {
+        // compute result before scrambling the state
+        let result = (self.state[0].wrapping_add(self.state[3]))
+            .rotate_left(23)
+            .wrapping_add(self.state[0]);
+
+        // scramble the state
+        let temp = self.state[1] << 17;
+        self.state[2] ^= self.state[0];
+        self.state[3] ^= self.state[1];
+        self.state[1] ^= self.state[2];
+        self.state[0] ^= self.state[3];
+        self.state[2] ^= temp;
+        self.state[3] = self.state[3].rotate_left(45);
+        result
     }
 }
