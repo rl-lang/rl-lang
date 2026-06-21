383c883115a927c57c9eb9224fe47d2aca363831
MohamedGonem <73464078+MohamedGonem@users.noreply.github.com>
2026-06-21 11:21:23 +0300

    apply clippy

--- src/interpreter/stdlib/random/xoshiro.rs
+++ src/interpreter/stdlib/random/xoshiro.rs
@@ -18,13 +18,13 @@ impl Xoshiro256 {
         // spread the seed into 4 u64
         let mut state = [0u64; 4];
         let mut x = seed;
-        for i in 0..4 {
+        for i in &mut state {
             // golden ratio * 2^64
             x = x.wrapping_add(0x9e3779b97f4a7c15);
             // magic constants
             x = (x ^ (x >> 30)).wrapping_mul(0xbf58476d1ce4e5b9);
             x = (x ^ (x >> 27)).wrapping_mul(0x94d049bb133111eb);
-            state[i] = x ^ (x >> 31);
+            *i = x ^ (x >> 31);
         }
 
         Self { state }
