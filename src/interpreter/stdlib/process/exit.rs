6bf3f245582a549babc874d0ea1c4a8f6da21c53
MohamedGonem <73464078+MohamedGonem@users.noreply.github.com>
2026-06-22 13:57:25 +0300

    add exit() function

---
+++ src/interpreter/stdlib/process/exit.rs
@@ -0,0 +1,8 @@
+use crate::{
+    interpreter::{evaluator::Evaluator, values::Value},
+    utils::{errors::Error, span::Span},
+};
+
+pub fn std_exit(_: &mut Evaluator, code: i64, _: Span) -> Result<Value, Error> {
+    std::process::exit(code as i32);
+}
