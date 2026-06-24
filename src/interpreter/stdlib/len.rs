1de1387e04a70fcb4f366c1af61035e7d1289b63
MohamedGonem <73464078+MohamedGonem@users.noreply.github.com>
2026-06-25 02:38:29 +0300

    add tuple to len

--- src/interpreter/stdlib/len.rs
+++ src/interpreter/stdlib/len.rs
@@ -7,6 +7,7 @@ pub fn std_len(eval: &mut Evaluator, v: Value, span: Span) -> Result<i64, Error>
     match v {
         Value::Values { items, .. } => Ok(items.len() as i64),
         Value::String(s) => Ok(s.len() as i64),
+        Value::Tuple(items) => Ok(items.len() as i64),
         other => Err(eval.err(
             format!(
                 "len() expects an array or string, got {}",
