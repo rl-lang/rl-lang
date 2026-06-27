4354fbac7d6536a66c7b708c0f71746635532117
MohamedGonem <73464078+MohamedGonem@users.noreply.github.com>
2026-06-27 14:35:55 +0300

    add move relative to cursor functions

--- src/interpreter/stdlib/terminal/mod.rs
+++ src/interpreter/stdlib/terminal/mod.rs
@@ -8,6 +8,7 @@ mod cursor_row;
 mod enter;
 mod leave;
 mod move_cursor;
+mod move_rel;
 
 use crate::interpreter::native::Module;
 
@@ -17,6 +18,10 @@ pub const KEYWORDS: &[&str] = &[
     "term_clear",
     "term_clear_line",
     "term_move",
+    "term_move_up",
+    "term_move_down",
+    "term_move_left",
+    "term_move_right",
     "term_move_to_col",
     "term_move_to_row",
 ];
@@ -33,4 +38,11 @@ pub fn module() -> Module {
         .with_raw_function("term_move", move_cursor::func)
         .with_raw_function("term_move_to_col", cursor_col::func)
         .with_raw_function("term_move_to_row", cursor_row::func)
+        // relative cursor
+        .with_raw_function("term_move_up", move_rel::std_term_move_up)
+        .with_raw_function("term_move_down", move_rel::std_term_move_down)
+        .with_raw_function("term_move_left", move_rel::std_term_move_left)
+        .with_raw_function("term_move_right", move_rel::std_term_move_right)
+        .with_raw_function("term_next_line", move_rel::std_term_next_line)
+        .with_raw_function("term_prev_line", move_rel::std_term_prev_line)
 }
