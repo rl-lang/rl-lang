041467d14d06e2661d600e403840d7169bab6668
MohamedGonem <73464078+MohamedGonem@users.noreply.github.com>
2026-06-27 14:21:18 +0300

    add term_leave()

--- src/interpreter/stdlib/terminal/mod.rs
+++ src/interpreter/stdlib/terminal/mod.rs
@@ -1,12 +1,18 @@
 //! `std::term` - full terminal control via crossterm.
 
 mod enter;
+mod leave;
+
 use crate::interpreter::native::Module;
+
 pub const KEYWORDS: &[&str] = &[
     "term_enter",
+    "term_leave",
 ];
 
 pub fn module() -> Module {
     Module::new("term")
+        // enter / leave
         .with_raw_function("term_enter", enter::func)
+        .with_raw_function("term_leave", leave::func)
 }
