974c64b1ae3938391d51f6c0b29e64c4acebe8bd
MohamedGonem <73464078+MohamedGonem@users.noreply.github.com>
2026-06-28 18:24:20 +0300

    refactor to_bin

--- src/interpreter/stdlib/types/to_bin.rs
+++ src/interpreter/stdlib/types/to_bin.rs
@@ -1,25 +1,26 @@
-use crate::{
-    interpreter::{evaluator::Evaluator, values::Value},
-    utils::{errors::Error, span::Span},
-};
+use crate::interpreter::{evaluator::Evaluator, values::Value};
 
-pub fn std_to_bin(eval: &mut Evaluator, value: Value, span: Span) -> Result<String, Error> {
-    match value {
-        Value::Byte(v) => Ok(format!("{:b}", v)),
-        Value::Integer(v) => Ok(format!("{:b}", v)),
+pub fn std_to_bin(_: &mut Evaluator, value: Value) -> Value {
+    let result = match value {
+        Value::Byte(v) => format!("{:b}", v),
+        Value::Integer(v) => format!("{:b}", v),
         Value::Bool(v) => {
             if v {
-                Ok("1".to_string())
+                "1".to_string()
             } else {
-                Ok("0".to_string())
+                "0".to_string()
             }
         }
-        Value::Char(v) => Ok(format!("{:b}", v as u32)),
-        Value::String(s) => Ok(s.bytes().map(|b| format!("{:b}", b)).collect::<String>()),
+        Value::Char(v) => format!("{:b}", v as u32),
+        Value::String(s) => s.bytes().map(|b| format!("{:b}", b)).collect::<String>(),
 
-        other => Err(eval.err(
-            format!("cannot parse \"{}\" as binary", other.type_name()),
-            span,
-        )),
-    }
+        other => {
+            return Value::Err(Box::new(Value::String(format!(
+                "cannot parse \"{}\" as binary",
+                other.type_name()
+            ))));
+        }
+    };
+
+    Value::Ok(Box::new(Value::String(result)))
 }
