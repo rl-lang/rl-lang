989ed3127de2310f41541ab95f44ae9264feb9b7
MohamedGonem <73464078+MohamedGonem@users.noreply.github.com>
2026-06-28 19:01:51 +0300

    update float

--- src/interpreter/stdlib/types/float.rs
+++ src/interpreter/stdlib/types/float.rs
@@ -1,23 +1,37 @@
-use crate::interpreter::{evaluator::Evaluator, values::Value};
+use crate::interpreter::{
+    evaluator::Evaluator,
+    stdlib::common::{verr, vf, vok, vs},
+    values::Value,
+};
 
 pub fn std_is_float(_: &mut Evaluator, value: Value) -> bool {
     matches!(value, Value::Float(_))
 }
 
-pub fn std_to_float(eval: &mut Evaluator, value: Value, span: Span) -> Result<f64, Error> {
-    match value {
-        Value::Float(f) => Ok(f),
-        Value::Integer(i) => Ok(i as f64),
-        Value::Byte(i) => Ok(i as f64),
-        Value::Bool(b) => Ok(if b { 1.0 } else { 0.0 }),
-        Value::String(s) => s
-            .trim()
-            .parse::<f64>()
-            .map_err(|_| eval.err(format!("cannot parse \"{}\" as float", s), span)),
+pub fn std_to_float(_: &mut Evaluator, value: Value) -> Value {
+    let result = match value {
+        Value::Float(f) => f,
+        Value::Integer(i) => i as f64,
+        Value::Byte(i) => i as f64,
+        Value::Bool(b) => {
+            if b {
+                1.0
+            } else {
+                0.0
+            }
+        }
+        Value::String(s) => match s.trim().parse::<f64>() {
+            Ok(f) => f,
+            Err(_) => return verr!(vs!(format!("cannot parse \"{}\" as float", s))),
+        },
 
-        other => Err(eval.err(
-            format!("cannot parse \"{}\" as float", other.type_name()),
-            span,
-        )),
-    }
+        other => {
+            return verr!(vs!(format!(
+                "cannot parse \"{}\" as float",
+                other.type_name()
+            )));
+        }
+    };
+
+    vok!(vf!(result))
 }
