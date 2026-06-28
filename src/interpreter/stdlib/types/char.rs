9545cf0c6e77c232f7c3d1b42f89bbc82678c9ba
MohamedGonem <73464078+MohamedGonem@users.noreply.github.com>
2026-06-28 18:56:45 +0300

    update char

--- src/interpreter/stdlib/types/char.rs
+++ src/interpreter/stdlib/types/char.rs
@@ -1,27 +1,39 @@
-use crate::interpreter::{evaluator::Evaluator, values::Value};
+use crate::interpreter::{
+    evaluator::Evaluator,
+    stdlib::common::{vc, verr, vok, vs},
+    values::Value,
+};
 
 pub fn std_is_char(_: &mut Evaluator, value: Value) -> bool {
     matches!(value, Value::Char(_))
 }
 
-pub fn std_to_char(eval: &mut Evaluator, value: Value, span: Span) -> Result<char, Error> {
-    match value {
-        Value::Char(c) => Ok(c),
-        Value::Integer(i) => char::from_u32(i as u32)
-            .ok_or_else(|| eval.err(format!("{} is not a valid unicode codepoint", i), span)),
-        Value::Byte(i) => char::from_u32(i as u32)
-            .ok_or_else(|| eval.err(format!("{} is not a valid unicode codepoint", i), span)),
+pub fn std_to_char(_: &mut Evaluator, value: Value) -> Value {
+    let result = match value {
+        Value::Char(c) => c,
+        Value::Integer(i) => match char::from_u32(i as u32) {
+            Some(c) => c,
+            None => return verr!(vs!(format!("{} is not a valid unicode codepoint", i))),
+        },
+        Value::Byte(i) => match char::from_u32(i as u32) {
+            Some(c) => c,
+            None => return verr!(vs!(format!("{} is not a valid unicode codepoint", i))),
+        },
         Value::String(s) => {
             let mut chars = s.chars();
             match (chars.next(), chars.next()) {
-                (Some(c), None) => Ok(c),
-                _ => Err(eval.err("string must be exactly one character".to_string(), span)),
+                (Some(c), None) => c,
+                _ => return verr!(vs!("string must be exactly one character".to_string())),
             }
         }
 
-        other => Err(eval.err(
-            format!("cannot parse \"{}\" as character", other.type_name()),
-            span,
-        )),
-    }
+        other => {
+            return verr!(vs!(format!(
+                "cannot parse \"{}\" as character",
+                other.type_name()
+            )));
+        }
+    };
+
+    vok!(vc!(result))
 }
