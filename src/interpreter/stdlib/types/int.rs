b968bd2ac983ec9bde7ca28f8f86126a5fbda257
MohamedGonem <73464078+MohamedGonem@users.noreply.github.com>
2026-06-28 19:18:03 +0300

    update int

--- src/interpreter/stdlib/types/int.rs
+++ src/interpreter/stdlib/types/int.rs
@@ -1,30 +1,47 @@
-use crate::interpreter::{evaluator::Evaluator, values::Value};
+use crate::interpreter::{
+    evaluator::Evaluator,
+    stdlib::common::{verr, vi, vok, vs},
+    values::Value,
+};
 
 pub fn std_is_int(_: &mut Evaluator, value: Value) -> bool {
     matches!(value, Value::Integer(_)) || matches!(value, Value::Byte(_))
 }
 
-pub fn std_to_int(eval: &mut Evaluator, value: Value, span: Span) -> Result<i64, Error> {
-    match value {
-        Value::Integer(v) => Ok(v),
-        Value::Byte(v) => Ok(v as i64),
-        Value::Float(v) => Ok(v as i64),
-        Value::Bool(v) => Ok(if v { 1 } else { 0 }),
-        Value::Char(v) => Ok(v as i64),
+pub fn std_to_int(_: &mut Evaluator, value: Value) -> Value {
+    let result = match value {
+        Value::Integer(v) => v,
+        Value::Byte(v) => v as i64,
+        Value::Float(v) => v as i64,
+        Value::Bool(v) => {
+            if v {
+                1
+            } else {
+                0
+            }
+        }
+        Value::Char(v) => v as i64,
         Value::String(s) => {
             let s = s.trim();
             if s.starts_with("0x") || s.starts_with("0X") {
-                i64::from_str_radix(&s[2..], 16)
-                    .map_err(|_| eval.err(format!("cannot parse \"{}\" as int", s), span))
+                match i64::from_str_radix(&s[2..], 16) {
+                    Ok(i) => i,
+                    Err(_) => return verr!(vs!(format!("cannot parse \"{}\" as int", s))),
+                }
             } else {
-                s.parse::<i64>()
-                    .map_err(|_| eval.err(format!("cannot parse \"{}\" as int", s), span))
+                match s.parse::<i64>() {
+                    Ok(i) => i,
+                    Err(_) => return verr!(vs!(format!("cannot parse \"{}\" as int", s))),
+                }
             }
         }
 
-        other => Err(eval.err(
-            format!("cannot parse \"{}\" as int", other.type_name()),
-            span,
-        )),
-    }
+        other => {
+            return verr!(vs!(format!(
+                "cannot parse \"{}\" as int",
+                other.type_name()
+            )));
+        }
+    };
+    vok!(vi!(result))
 }
