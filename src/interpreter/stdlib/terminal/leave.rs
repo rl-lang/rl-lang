826a0052fbc6e6668bcdf0c2ff4d451267720a5b
MohamedGonem <73464078+MohamedGonem@users.noreply.github.com>
2026-06-29 12:51:26 +0300

    fix term_leave not flushing correctly

--- src/interpreter/stdlib/terminal/leave.rs
+++ src/interpreter/stdlib/terminal/leave.rs
@@ -1,17 +1,21 @@
 use crate::interpreter::stdlib::common::check_arity;
 use crate::interpreter::{evaluator::Evaluator, values::Value};
 use crate::utils::{errors::Error, span::Span};
+use crossterm::cursor::Show;
 use crossterm::{
     execute,
     terminal::{LeaveAlternateScreen, disable_raw_mode},
 };
-use std::io::stdout;
+use std::io::{Write, stderr, stdout};
 
 pub fn func(eval: &mut Evaluator, args: Vec<Value>, span: Span) -> Result<Value, Error> {
     check_arity(&args, 0, "term_leave", span)?;
 
+    let _ = stdout().flush();
+    let _ = stderr().flush();
     disable_raw_mode().map_err(|e| eval.err(format!("term_leave(): {}", e), span))?;
-    execute!(stdout(), LeaveAlternateScreen)
+    execute!(stdout(), Show, LeaveAlternateScreen)
         .map_err(|e| eval.err(format!("term_leave(): {}", e), span))?;
+    let _ = stdout().flush();
     Ok(Value::Ok(Box::new(Value::Null)))
 }
