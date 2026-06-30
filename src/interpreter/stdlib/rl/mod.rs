8919ecce84a1b7287bedee190a5d8067928d716a
MohamedGonem <73464078+MohamedGonem@users.noreply.github.com>
2026-06-30 18:26:06 +0300

    add rl_version and source_name functions

--- src/interpreter/stdlib/rl/mod.rs
+++ src/interpreter/stdlib/rl/mod.rs
@@ -4,10 +4,19 @@ mod check;
 mod eval;
 mod eval_isolated;
 mod lex;
+mod rl_version;
+mod source_name;
 
 use crate::interpreter::native::Module;
 
-pub const KEYWORDS: &[&str] = &["lex", "eval", "eval_isolated", "check"];
+pub const KEYWORDS: &[&str] = &[
+    "lex",
+    "eval",
+    "eval_isolated",
+    "check",
+    "rl_version",
+    "source_name",
+];
 
 pub fn module() -> Module {
     Module::new("rl")
@@ -15,4 +24,6 @@ pub fn module() -> Module {
         .with_function("eval", eval::func)
         .with_function("eval_isolated", eval_isolated::func)
         .with_function("check", check::func)
+        .with_function("rl_version", rl_version::func)
+        .with_function("source_name", source_name::func)
 }
