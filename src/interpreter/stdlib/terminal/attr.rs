5e232ebd20ac0f98716cdb9db170ad51eda9a152
MohamedGonem <73464078+MohamedGonem@users.noreply.github.com>
2026-07-02 09:30:24 +0300

    update attr.rs to use Result

--- src/interpreter/stdlib/terminal/attr.rs
+++ src/interpreter/stdlib/terminal/attr.rs
@@ -1,70 +1,46 @@
-use crate::interpreter::stdlib::common::check_arity;
+use crate::interpreter::stdlib::common::{verr, vnl, vok, vs};
 use crate::interpreter::{evaluator::Evaluator, values::Value};
-use crate::utils::{errors::Error, span::Span};
 use crossterm::{
     execute,
     style::{Attribute, SetAttribute},
 };
 use std::io::stdout;
 
-fn set_attr(
-    attr: Attribute,
-    name: &str,
-    eval: &mut Evaluator,
-    args: Vec<Value>,
-    span: Span,
-) -> Result<Value, Error> {
-    check_arity(&args, 0, name, span)?;
-
-    execute!(stdout(), SetAttribute(attr))
-        .map_err(|e| eval.err(format!("{}(): {}", name, e), span))?;
-    Ok(Value::Ok(Box::new(Value::Null)))
+fn set_attr(attr: Attribute, name: &str) -> Value {
+    match execute!(stdout(), SetAttribute(attr)) {
+        Err(e) => verr!(vs!(format!("{}(): {}", name, e))),
+        Ok(_) => vok!(vnl!()),
+    }
 }
 
-pub fn std_term_bold(eval: &mut Evaluator, args: Vec<Value>, span: Span) -> Result<Value, Error> {
-    set_attr(Attribute::Bold, "term_bold", eval, args, span)
+pub fn std_term_bold(_: &mut Evaluator) -> Value {
+    set_attr(Attribute::Bold, "term_bold")
 }
 
-pub fn std_term_dim(eval: &mut Evaluator, args: Vec<Value>, span: Span) -> Result<Value, Error> {
-    set_attr(Attribute::Dim, "term_dim", eval, args, span)
+pub fn std_term_dim(_: &mut Evaluator) -> Value {
+    set_attr(Attribute::Dim, "term_dim")
 }
 
-pub fn std_term_italic(eval: &mut Evaluator, args: Vec<Value>, span: Span) -> Result<Value, Error> {
-    set_attr(Attribute::Italic, "term_italic", eval, args, span)
+pub fn std_term_italic(_: &mut Evaluator) -> Value {
+    set_attr(Attribute::Italic, "term_italic")
 }
 
-pub fn std_term_underline(
-    eval: &mut Evaluator,
-    args: Vec<Value>,
-    span: Span,
-) -> Result<Value, Error> {
-    set_attr(Attribute::Underlined, "term_underline", eval, args, span)
+pub fn std_term_underline(_: &mut Evaluator) -> Value {
+    set_attr(Attribute::Underlined, "term_underline")
 }
 
-pub fn std_term_blink(eval: &mut Evaluator, args: Vec<Value>, span: Span) -> Result<Value, Error> {
-    set_attr(Attribute::SlowBlink, "term_blink", eval, args, span)
+pub fn std_term_blink(_: &mut Evaluator) -> Value {
+    set_attr(Attribute::SlowBlink, "term_blink")
 }
 
-pub fn std_term_reverse(
-    eval: &mut Evaluator,
-    args: Vec<Value>,
-    span: Span,
-) -> Result<Value, Error> {
-    set_attr(Attribute::Reverse, "term_reverse", eval, args, span)
+pub fn std_term_reverse(_: &mut Evaluator) -> Value {
+    set_attr(Attribute::Reverse, "term_reverse")
 }
 
-pub fn std_term_crossed_out(
-    eval: &mut Evaluator,
-    args: Vec<Value>,
-    span: Span,
-) -> Result<Value, Error> {
-    set_attr(Attribute::CrossedOut, "term_crossed_out", eval, args, span)
+pub fn std_term_crossed_out(_: &mut Evaluator) -> Value {
+    set_attr(Attribute::CrossedOut, "term_crossed_out")
 }
 
-pub fn std_term_reset_attr(
-    eval: &mut Evaluator,
-    args: Vec<Value>,
-    span: Span,
-) -> Result<Value, Error> {
-    set_attr(Attribute::Reset, "term_reset_attr", eval, args, span)
+pub fn std_term_reset_attr(_: &mut Evaluator) -> Value {
+    set_attr(Attribute::Reset, "term_reset_attr")
 }
