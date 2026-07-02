795cfd39c917a088e6da89e206a5397f47f367da
MohamedGonem <73464078+MohamedGonem@users.noreply.github.com>
2026-07-02 17:26:55 +0300

    update factor

--- src/parser/expressions/factor.rs
+++ src/parser/expressions/factor.rs
@@ -1,5 +1,5 @@
 use crate::{
-    ast::nodes::{Expression, ExpressionKind},
+    ast::{ExprId, nodes::ExpressionKind},
     lexer::tokentypes::TokenType,
     parser::parser_logic::Parser,
     utils::errors::Error,
@@ -9,19 +9,19 @@ impl Parser {
     /// Parses multiplicative expressions: `*` and `/`.
     ///
     /// Left-associative: `a * b / c` is `(a * b) / c`.
-    pub fn parse_factor(&mut self) -> Result<Expression, Error> {
+    pub fn parse_factor(&mut self) -> Result<ExprId, Error> {
         let mut left = self.parse_unary()?;
         while self.match_type(&[TokenType::Star, TokenType::Slash]) {
             while self.match_type(&[TokenType::Newline]) {}
             let operator = self.previous();
             while self.match_type(&[TokenType::Newline]) {}
             let right = self.parse_unary()?;
-            let span = left.span.join(right.span);
-            left = Expression::new(
+            let span = self.expr_span(left).join(self.expr_span(right));
+            left = self.ast.alloc_expr(
                 ExpressionKind::Binary {
-                    left: Box::new(left),
+                    left: left,
                     operator,
-                    right: Box::new(right),
+                    right: right,
                 },
                 span,
             );
