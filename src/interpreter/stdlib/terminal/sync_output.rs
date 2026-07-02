81da7a98dbdbdd94e39349fc4fa5956d35dcc029
MohamedGonem <73464078+MohamedGonem@users.noreply.github.com>
2026-07-02 12:49:53 +0300

    fix sync functions

--- src/interpreter/stdlib/terminal/sync_output.rs
+++ src/interpreter/stdlib/terminal/sync_output.rs
@@ -1,32 +1,22 @@
-use crate::interpreter::stdlib::common::check_arity;
-use crate::interpreter::{evaluator::Evaluator, values::Value};
-use crate::utils::{errors::Error, span::Span};
+use crate::interpreter::{
+    evaluator::Evaluator,
+    stdlib::common::{verr, vnl, vok, vs},
+    values::Value,
+};
 use crossterm::{
     execute,
     terminal::{BeginSynchronizedUpdate, EndSynchronizedUpdate},
 };
 use std::io::stdout;
 
-pub fn std_term_begin_sync(
-    eval: &mut Evaluator,
-    args: Vec<Value>,
-    span: Span,
-) -> Result<Value, Error> {
-    check_arity(&args, 0, "term_begin_sync", span)?;
-
+pub fn std_term_begin_sync(_: &mut Evaluator) -> Value {
     execute!(stdout(), BeginSynchronizedUpdate)
-        .map_err(|e| eval.err(format!("term_begin_sync(): {}", e), span))?;
-    Ok(Value::Ok(Box::new(Value::Null)))
+        .map_err(|e| return verr!(vs!(format!("term_begin_sync(): {}", e))));
+    vok!(vnl!())
 }
 
-pub fn std_term_end_sync(
-    eval: &mut Evaluator,
-    args: Vec<Value>,
-    span: Span,
-) -> Result<Value, Error> {
-    check_arity(&args, 0, "term_end_sync", span)?;
-
+pub fn std_term_end_sync(_: &mut Evaluator) -> Value {
     execute!(stdout(), EndSynchronizedUpdate)
-        .map_err(|e| eval.err(format!("term_end_sync(): {}", e), span))?;
-    Ok(Value::Ok(Box::new(Value::Null)))
+        .map_err(|e| return verr!(vs!(format!("term_end_sync(): {}", e))));
+    vok!(vnl!())
 }
