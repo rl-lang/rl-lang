af8045cda75ecbda9d4e34491c06929b77b805dd
MohamedGonem <73464078+MohamedGonem@users.noreply.github.com>
2026-07-02 17:28:55 +0300

    update equality

--- src/parser/expressions/equality.rs
+++ src/parser/expressions/equality.rs
@@ -1,5 +1,5 @@
 use crate::{
-    ast::nodes::{Expression, ExpressionKind},
+    ast::{ExprId, nodes::ExpressionKind},
     lexer::tokentypes::TokenType,
     parser::parser_logic::Parser,
     utils::errors::Error,
@@ -9,19 +9,19 @@ impl Parser {
     /// Parses `==` and `!=` binary expressions (lowest precedence).
     ///
     /// Left-associative: `a == b != c` is `(a == b) != c`.
-    pub fn parse_equality(&mut self) -> Result<Expression, Error> {
+    pub fn parse_equality(&mut self) -> Result<ExprId, Error> {
         let mut left = self.parse_comparsion()?;
         while self.match_type(&[TokenType::BangEqual, TokenType::Compare]) {
             while self.match_type(&[TokenType::Newline]) {}
             let operator = self.previous();
             while self.match_type(&[TokenType::Newline]) {}
             let right = self.parse_comparsion()?;
-            let span = left.span.join(right.span);
-            left = Expression::new(
+            let span = self.expr_span(left).join(self.expr_span(right));
+            left = self.ast.alloc_expr(
                 ExpressionKind::Binary {
-                    left: Box::new(left),
+                    left,
                     operator,
-                    right: Box::new(right),
+                    right,
                 },
                 span,
             );
