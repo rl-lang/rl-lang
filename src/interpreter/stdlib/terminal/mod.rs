1501e1133af2d5cbc22a13a6284f722143b5f5f1
MohamedGonem <73464078+MohamedGonem@users.noreply.github.com>
2026-07-02 12:49:02 +0300

    fix wrap

--- src/interpreter/stdlib/terminal/mod.rs
+++ src/interpreter/stdlib/terminal/mod.rs
@@ -99,25 +99,25 @@ pub fn module() -> Module {
         .with_function("term_next_line", move_rel::std_term_next_line)
         .with_function("term_prev_line", move_rel::std_term_prev_line)
         // save / restore
-        .with_raw_function("term_save_cursor", save_cursor::func)
-        .with_raw_function("term_restore_cursor", restore_cursor::func)
+        .with_function("term_save_cursor", save_cursor::func)
+        .with_function("term_restore_cursor", restore_cursor::func)
         // show / hide
         .with_function("term_hide_cursor", hide_cursor::func)
-        .with_raw_function("term_show_cursor", show_cursor::func)
+        .with_function("term_show_cursor", show_cursor::func)
         // size / title
-        .with_raw_function("term_get_size", size::std_term_get_size)
-        .with_raw_function("term_set_size", size::std_term_set_size)
-        .with_raw_function("term_set_title", set_title::func)
+        .with_function("term_get_size", size::std_term_get_size)
+        .with_function("term_set_size", size::std_term_set_size)
+        .with_function("term_set_title", set_title::func)
         // scroll
-        .with_raw_function("term_scroll_up", scroll::std_term_scroll_up)
-        .with_raw_function("term_scroll_down", scroll::std_term_scroll_down)
+        .with_function("term_scroll_up", scroll::std_term_scroll_up)
+        .with_function("term_scroll_down", scroll::std_term_scroll_down)
         // output
-        .with_raw_function("term_print", print::func)
+        .with_function("term_print", print::func)
         .with_function("term_flush", flush::func)
         // rgb color
-        .with_raw_function("term_set_fg", set_fg::func)
-        .with_raw_function("term_set_bg", set_bg::func)
-        .with_raw_function("term_reset_color", reset_color::func)
+        .with_function("term_set_fg", set_fg::func)
+        .with_function("term_set_bg", set_bg::func)
+        .with_function("term_reset_color", reset_color::func)
         // named color
         .with_function("term_fg", named_color::std_term_fg)
         .with_function("term_bg", named_color::std_term_bg)
@@ -131,15 +131,15 @@ pub fn module() -> Module {
         .with_function("term_crossed_out", attr::std_term_crossed_out)
         .with_function("term_reset_attr", attr::std_term_reset_attr)
         // line wrap
-        .with_raw_function("term_enable_wrap", wrap::std_term_enable_wrap)
-        .with_raw_function("term_disable_wrap", wrap::std_term_disable_wrap)
+        .with_function("term_enable_wrap", wrap::std_term_enable_wrap)
+        .with_function("term_disable_wrap", wrap::std_term_disable_wrap)
         // synchronized output
-        .with_raw_function("term_begin_sync", sync_output::std_term_begin_sync)
-        .with_raw_function("term_end_sync", sync_output::std_term_end_sync)
+        .with_function("term_begin_sync", sync_output::std_term_begin_sync)
+        .with_function("term_end_sync", sync_output::std_term_end_sync)
         // mouse
         .with_function("term_enable_mouse", mouse::std_term_enable_mouse)
         .with_function("term_disable_mouse", mouse::std_term_disable_mouse)
         // input
-        .with_raw_function("term_read_key", read_key::func)
-        .with_raw_function("term_poll", poll::func)
+        .with_function("term_read_key", read_key::func)
+        .with_function("term_poll", poll::func)
 }
--- src/interpreter/stdlib/terminal/wrap.rs
+++ src/interpreter/stdlib/terminal/wrap.rs
@@ -1,32 +1,20 @@
-use crate::interpreter::stdlib::common::check_arity;
+use crate::interpreter::stdlib::common::{verr, vnl, vok, vs};
 use crate::interpreter::{evaluator::Evaluator, values::Value};
-use crate::utils::{errors::Error, span::Span};
+
 use crossterm::{
     execute,
     terminal::{DisableLineWrap, EnableLineWrap},
 };
 use std::io::stdout;
 
-pub fn std_term_enable_wrap(
-    eval: &mut Evaluator,
-    args: Vec<Value>,
-    span: Span,
-) -> Result<Value, Error> {
-    check_arity(&args, 0, "term_enable_wrap", span)?;
-
+pub fn std_term_enable_wrap(_: &mut Evaluator) -> Value {
     execute!(stdout(), EnableLineWrap)
-        .map_err(|e| eval.err(format!("term_enable_wrap(): {}", e), span))?;
-    Ok(Value::Ok(Box::new(Value::Null)))
+        .map_err(|e| return verr!(vs!(format!("term_enable_wrap(): {}", e))));
+    vok!(vnl!())
 }
 
-pub fn std_term_disable_wrap(
-    eval: &mut Evaluator,
-    args: Vec<Value>,
-    span: Span,
-) -> Result<Value, Error> {
-    check_arity(&args, 0, "term_disable_wrap", span)?;
-
+pub fn std_term_disable_wrap(_: &mut Evaluator) -> Value {
     execute!(stdout(), DisableLineWrap)
-        .map_err(|e| eval.err(format!("term_disable_wrap(): {}", e), span))?;
-    Ok(Value::Ok(Box::new(Value::Null)))
+        .map_err(|e| return verr!(vs!(format!("term_disable_wrap(): {}", e))));
+    vok!(vnl!())
 }
