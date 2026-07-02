d779cbc05fe90ea53396c24243d459c1d500e8c4
MohamedGonem <73464078+MohamedGonem@users.noreply.github.com>
2026-07-02 14:28:46 +0300

    add arena module for ast for expressions and statements

--- src/ast/mod.rs
+++ src/ast/mod.rs
@@ -11,8 +11,41 @@
 //!
 //! [`Expression`]: nodes::Expression
 //! [`Statement`]: statements::Statement
+
+use crate::{
+    ast::{
+        arena::{Arena, Id},
+        nodes::{Expression, ExpressionKind},
+        statements::{Statement, StatementKind},
+    },
+    utils::span::Span,
+};
 pub mod nodes;
 pub mod statements;
 // pub mod variables;
 pub type ScopeMap = Vec<Vec<String>>;
+mod arena;
 pub mod macros;
+
+pub struct Ast {
+    pub exprs: Arena<Expression>,
+    pub stmts: Arena<Statement>,
+}
+
+pub type ExprId = Id<Expression>;
+pub type StmtId = Id<Statement>;
+
+impl Ast {
+    pub fn new() -> Self {
+        Self {
+            exprs: Arena::new(),
+            stmts: Arena::new(),
+        }
+    }
+    pub fn alloc_expr(&mut self, kind: ExpressionKind, span: Span) -> ExprId {
+        self.exprs.alloc(Expression { kind, span })
+    }
+    pub fn alloc_stmt(&mut self, kind: StatementKind, span: Span) -> StmtId {
+        self.stmts.alloc(Statement { kind, span })
+    }
+}
