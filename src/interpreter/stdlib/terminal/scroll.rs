873ec637a0486823cc38e7489582577238577a52
MohamedGonem <73464078+MohamedGonem@users.noreply.github.com>
2026-07-02 13:01:21 +0300

    update scroll functions

--- src/interpreter/stdlib/terminal/scroll.rs
+++ src/interpreter/stdlib/terminal/scroll.rs
@@ -1,35 +1,30 @@
-use crate::interpreter::stdlib::common::check_arity;
+use crate::interpreter::stdlib::common::{verr, vnl, vok, vs};
 use crate::interpreter::stdlib::terminal::common::extract_u16;
 use crate::interpreter::{evaluator::Evaluator, values::Value};
-use crate::utils::{errors::Error, span::Span};
 use crossterm::{
     execute,
     terminal::{ScrollDown, ScrollUp},
 };
 use std::io::stdout;
 
-pub fn std_term_scroll_up(
-    eval: &mut Evaluator,
-    args: Vec<Value>,
-    span: Span,
-) -> Result<Value, Error> {
-    check_arity(&args, 1, "term_scroll_down", span)?;
+pub fn std_term_scroll_up(_: &mut Evaluator, arg: Value) -> Value {
+    let n = match extract_u16(arg, "n") {
+        Ok(v) => v,
+        Err(e) => return verr!(vs!(e)),
+    };
 
-    let n = extract_u16(args.into_iter().next().unwrap(), "n", eval, span)?;
     execute!(stdout(), ScrollUp(n))
-        .map_err(|e| eval.err(format!("term_scroll_up(): {}", e), span))?;
-    Ok(Value::Ok(Box::new(Value::Null)))
+        .map_err(|e| return verr!(vs!(format!("term_scroll_up(): {}", e))));
+    vok!(vnl!())
 }
 
-pub fn std_term_scroll_down(
-    eval: &mut Evaluator,
-    args: Vec<Value>,
-    span: Span,
-) -> Result<Value, Error> {
-    check_arity(&args, 1, "term_scroll_down", span)?;
+pub fn std_term_scroll_down(_: &mut Evaluator, arg: Value) -> Value {
+    let n = match extract_u16(arg, "n") {
+        Ok(v) => v,
+        Err(e) => return verr!(vs!(e)),
+    };
 
-    let n = extract_u16(args.into_iter().next().unwrap(), "n", eval, span)?;
     execute!(stdout(), ScrollDown(n))
-        .map_err(|e| eval.err(format!("term_scroll_down(): {}", e), span))?;
-    Ok(Value::Ok(Box::new(Value::Null)))
+        .map_err(|e| return verr!(vs!(format!("term_scroll_down(): {}", e))));
+    vok!(vnl!())
 }
