440b991cd204d7561ab164d24f136128a3d1a1d5
MohamedGonem <73464078+MohamedGonem@users.noreply.github.com>
2026-07-02 12:54:11 +0300

    update cursor functions

--- src/interpreter/stdlib/terminal/restore_cursor.rs
+++ src/interpreter/stdlib/terminal/restore_cursor.rs
@@ -1,13 +1,13 @@
-use crate::interpreter::stdlib::common::check_arity;
-use crate::interpreter::{evaluator::Evaluator, values::Value};
-use crate::utils::{errors::Error, span::Span};
+use crate::interpreter::{
+    evaluator::Evaluator,
+    stdlib::common::{verr, vnl, vok, vs},
+    values::Value,
+};
 use crossterm::{cursor::RestorePosition, execute};
 use std::io::stdout;
 
-pub fn func(eval: &mut Evaluator, args: Vec<Value>, span: Span) -> Result<Value, Error> {
-    check_arity(&args, 0, "term_save_cursor", span)?;
-
+pub fn func(_: &mut Evaluator) -> Value {
     execute!(stdout(), RestorePosition)
-        .map_err(|e| eval.err(format!("term_restore_cursor(): {}", e), span))?;
-    Ok(Value::Ok(Box::new(Value::Null)))
+        .map_err(|e| return verr!(vs!(format!("term_restore_cursor(): {}", e))));
+    vok!(vnl!())
 }
--- src/interpreter/stdlib/terminal/show_cursor.rs
+++ src/interpreter/stdlib/terminal/show_cursor.rs
@@ -1,12 +1,9 @@
-use crate::interpreter::stdlib::common::check_arity;
+use crate::interpreter::stdlib::common::{verr, vnl, vok, vs};
 use crate::interpreter::{evaluator::Evaluator, values::Value};
-use crate::utils::{errors::Error, span::Span};
 use crossterm::{cursor::Show, execute};
 use std::io::stdout;
 
-pub fn func(eval: &mut Evaluator, args: Vec<Value>, span: Span) -> Result<Value, Error> {
-    check_arity(&args, 0, "term_show_cursor", span)?;
-
-    execute!(stdout(), Show).map_err(|e| eval.err(format!("term_show_cursor(): {}", e), span))?;
-    Ok(Value::Ok(Box::new(Value::Null)))
+pub fn func(_: &mut Evaluator) -> Value {
+    execute!(stdout(), Show).map_err(|e| return verr!(vs!(format!("term_show_cursor(): {}", e))));
+    vok!(vnl!())
 }
