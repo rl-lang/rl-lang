58527261d3da525e3faee94992164c7b8929104c
MohamedGonem <73464078+MohamedGonem@users.noreply.github.com>
2026-07-06 14:13:03 +0300

    create two helper functions for http

---
+++ src/interpreter/stdlib/http/common.rs
@@ -0,0 +1,32 @@
+use crate::interpreter::{
+    evaluator::Evaluator,
+    stdlib::{
+        common::{verr, vi, vok, vs},
+        http::HttpHandle,
+    },
+    values::Value,
+};
+
+pub fn ureq_result_to_value(url: &str, result: Result<ureq::Response, ureq::Error>) -> Value {
+    match result {
+        Ok(response) => {
+            let status = response.status() as i64;
+            let body = response.into_string().unwrap_or_default();
+            vok!(Value::Tuple(vec![vi!(status), vs!(body)]))
+        }
+        Err(ureq::Error::Status(code, response)) => {
+            let body = response.into_string().unwrap_or_default();
+            vok!(Value::Tuple(vec![vi!(code as i64), vs!(body)]))
+        }
+        Err(e) => {
+            verr!(vs!(format!("{}: {}", url, e)))
+        }
+    }
+}
+
+pub fn insert_handle(eval: &mut Evaluator, handle: HttpHandle) -> i64 {
+    let id = eval.http_next_handle;
+    eval.http_next_handle += 1;
+    eval.http_handles.insert(id, handle);
+    id
+}
