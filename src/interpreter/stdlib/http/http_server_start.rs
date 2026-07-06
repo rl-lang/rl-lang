4226c8c1199c68683f4a817b501f4be104ec8750
MohamedGonem <73464078+MohamedGonem@users.noreply.github.com>
2026-07-06 14:13:46 +0300

    add function http_server_start

---
+++ src/interpreter/stdlib/http/http_server_start.rs
@@ -0,0 +1,17 @@
+use crate::interpreter::{
+    evaluator::Evaluator,
+    stdlib::{
+        common::{verr, vi, vok, vs},
+        http::{HttpHandle, common::insert_handle},
+    },
+    values::Value,
+};
+pub fn func(eval: &mut Evaluator, addr: String) -> Value {
+    match tiny_http::Server::http(&addr) {
+        Ok(server) => {
+            let id = insert_handle(eval, HttpHandle::Server(server));
+            vok!(vi!(id))
+        }
+        Err(e) => verr!(vs!(format!("http_server_start(\"{}\"): {}", addr, e))),
+    }
+}
