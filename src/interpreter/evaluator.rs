e2f91da04b0d5d410ee71134cd6bfb5e4945d359
MohamedGonem <73464078+MohamedGonem@users.noreply.github.com>
2026-07-06 20:12:55 +0300

    apply clippy

--- src/interpreter/evaluator.rs
+++ src/interpreter/evaluator.rs
@@ -886,14 +886,13 @@ impl Evaluator {
                     _ => unreachable!(),
                 };
 
-                if let Some(declared_variants) = self.tags.get(&enum_name) {
-                    if !declared_variants.contains(&variant) {
+                if let Some(declared_variants) = self.tags.get(&enum_name)
+                    && !declared_variants.contains(&variant) {
                         return Err(self.err(
                             format!("tag `{}` has no variant `{}`", enum_name, variant),
                             span,
                         ));
                     }
-                }
 
                 Ok(Value::Enum {
                     name: enum_name,
