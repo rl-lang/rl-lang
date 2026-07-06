4a73948bb72b7108824831a983b86f4e1f6407a9
MohamedGonem <73464078+MohamedGonem@users.noreply.github.com>
2026-07-06 07:35:33 +0300

    add tcp_local_addr function

--- src/interpreter/stdlib/net/mod.rs
+++ src/interpreter/stdlib/net/mod.rs
@@ -43,6 +43,7 @@ pub fn module() -> Module {
         .with_function("tcp_listen", tcp_listen::func)
         .with_function("tcp_accept", tcp_accept::func)
         .with_function("tcp_connect", tcp_connect::func)
+        .with_function("tcp_local_addr", tcp_local_addr::func)
         .with_function("tcp_close", tcp_close::func)
         .with_function("resolve", resolve::func)
 }
---
+++ src/interpreter/stdlib/net/tcp_local_addr.rs
@@ -0,0 +1,34 @@
+use crate::interpreter::{
+    evaluator::Evaluator,
+    stdlib::{
+        common::{extract_number, verr, vok, vs},
+        net::NetHandle,
+    },
+    values::Value,
+};
+
+pub fn func(eval: &mut Evaluator, id: Value) -> Value {
+    let id = match extract_number(id, "tcp_local_addr") {
+        Ok(a) if a as i64 >= 0 => a as i64,
+        Ok(_) => {
+            return verr!(vs!(
+                "tcp_local_addr: id handle cannot be negative".to_string()
+            ));
+        }
+        Err(e) => return verr!(vs!(format!("tcp_accept: {}", e))),
+    };
+    let stream = match eval.net_handles.get(&id) {
+        Some(NetHandle::TcpStream(stream)) => stream,
+        Some(_) => {
+            return verr!(vs!(format!(
+                "tcp_local_addr: handle {} is not a TCP stream",
+                id
+            )));
+        }
+        None => return verr!(vs!(format!("tcp_local_addr: unknown handle {}", id))),
+    };
+    match stream.local_addr() {
+        Ok(addr) => vok!(vs!(addr.to_string())),
+        Err(e) => verr!(vs!(format!("tcp_local_addr: {}", e))),
+    }
+}
