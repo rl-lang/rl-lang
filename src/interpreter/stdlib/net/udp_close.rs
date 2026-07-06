99a7b6c41bc5d49e67a97bc2489efc25897b4e93
MohamedGonem <73464078+MohamedGonem@users.noreply.github.com>
2026-07-06 09:51:36 +0300

    add udp_close function

---
+++ src/interpreter/stdlib/net/udp_close.rs
@@ -0,0 +1,27 @@
+use crate::interpreter::{
+    evaluator::Evaluator,
+    stdlib::{
+        common::{extract_number, verr, vnl, vok, vs},
+        net::NetHandle,
+    },
+    values::Value,
+};
+
+pub fn func(eval: &mut Evaluator, id: Value) -> Value {
+    let id = match extract_number(id, "udp_close") {
+        Ok(a) if a as i64 >= 0 => a as i64,
+        Ok(_) => return verr!(vs!("tcp_accept: id handle cannot be negative".to_string())),
+        Err(e) => return verr!(vs!(format!("tcp_accept: {}", e))),
+    };
+    match eval.net_handles.get(&id) {
+        Some(NetHandle::UdpSocket(_)) => {
+            eval.net_handles.remove(&id);
+            vok!(vnl!())
+        }
+        Some(_) => verr!(vs!(format!(
+            "udp_close(): handle {} is not a UDP socket",
+            id
+        ))),
+        None => verr!(vs!(format!("udp_close(): unknown handle {}", id))),
+    }
+}
