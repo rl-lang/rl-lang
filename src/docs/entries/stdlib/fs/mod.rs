8b575c48a815e036e2736cf839d9a0c28bfb4e51
MohamedGonem <73464078+MohamedGonem@users.noreply.github.com>
2026-07-08 13:41:40 +0300

    update docs correctly

--- src/docs/entries/stdlib/fs/mod.rs
+++ src/docs/entries/stdlib/fs/mod.rs
@@ -25,12 +25,23 @@ static FUNCTIONS: &[&FnEntry] = &[
 static COPY_FILE: FnEntry = FnEntry {
     signature: "copy_file(src, dst)",
     description: "copies a file from src to dst, returns the number of bytes copied",
-    example: "get std::fs::copy_file\n\ncopy_file(\"a.txt\", \"b.txt\") // 1024",
+    example: r#"
+get std::fs::copy_file
+
+copy_file("a.txt", "b.txt")?"#,
     expected_output: None,
-    returns: "",
-    errors: None,
-    see_also: &[],
-    since: None,
+    returns: "result[int]",
+    errors: Some(
+        r#"
+Will return errors on the following:
+
+- `src` does not exists
+- `src` not regular file nor symlink to regular file
+- the current process does not have permission rights to read from `src` and write to `dst`
+- the parent directory of `dst` does not exists"#,
+    ),
+    see_also: &["move_file"],
+    since: Some("v0.1.5"),
 };
 
 static FILE_MODIFIED: FnEntry = FnEntry {
