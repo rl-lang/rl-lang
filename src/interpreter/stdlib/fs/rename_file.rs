44423ddc81573f77f3cc1956dbb17a63160d2f54
MohamedGonem <73464078+MohamedGonem@users.noreply.github.com>
2026-07-08 14:59:00 +0300

    change rename_file to return result[string] instead

--- src/interpreter/stdlib/fs/rename_file.rs
+++ src/interpreter/stdlib/fs/rename_file.rs
@@ -1,31 +1,24 @@
-use crate::{
-    interpreter::{evaluator::Evaluator, values::Value},
-    utils::{errors::Error, span::Span},
+use crate::interpreter::{
+    evaluator::Evaluator,
+    stdlib::common::{verr, vok, vs},
+    values::Value,
 };
 
-pub fn std_rename_file(
-    eval: &mut Evaluator,
-    path: String,
-    new_name: String,
-    span: Span,
-) -> Result<Value, Error> {
+pub fn std_rename_file(_: &mut Evaluator, path: String, new_name: String) -> Value {
     let old_path = std::path::Path::new(&path);
     let new_path = match old_path.parent() {
         Some(parent) => parent.join(&new_name),
         None => std::path::PathBuf::from(&new_name),
     };
 
-    std::fs::rename(old_path, &new_path).map_err(|e| {
-        eval.err(
-            format!(
-                "rename_file(): failed to rename \"{}\" to \"{}\": {}",
-                path,
-                new_path.to_string_lossy(),
-                e
-            ),
-            span,
-        )
-    })?;
+    if let Err(e) = std::fs::rename(old_path, &new_path) {
+        return verr!(vs!(format!(
+            "rename_file(): failed to rename \"{}\" to \"{}\": {}",
+            path,
+            new_path.to_string_lossy(),
+            e
+        )));
+    };
 
-    Ok(Value::String(new_path.to_string_lossy().to_string()))
+    vok!(vs!(new_path.to_string_lossy().to_string()))
 }
