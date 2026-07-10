74465ebf8716bd2cb7d734ad048f23855e69fa6a
MohamedGonem <73464078+MohamedGonem@users.noreply.github.com>
2026-07-10 18:49:28 +0300

    change mkdir to return result[null]

--- src/interpreter/stdlib/fs/mkdir.rs
+++ src/interpreter/stdlib/fs/mkdir.rs
@@ -1,14 +1,12 @@
-use crate::{
-    interpreter::{evaluator::Evaluator, values::Value},
-    utils::{errors::Error, span::Span},
+use crate::interpreter::{
+    evaluator::Evaluator,
+    stdlib::common::{verr, vnl, vok, vs},
+    values::Value,
 };
 
-pub fn std_mkdir(eval: &mut Evaluator, path: String, span: Span) -> Result<Value, Error> {
-    std::fs::create_dir(&path).map_err(|e| {
-        eval.err(
-            format!("mkdir(): failed to create \"{}\": {}", path, e),
-            span,
-        )
-    })?;
-    Ok(Value::Null)
+pub fn std_mkdir(_: &mut Evaluator, path: String) -> Value {
+    if let Err(e) = std::fs::create_dir(&path) {
+        return verr!(vs!(format!("mkdir: failed to create \"{}\": {}", path, e)));
+    };
+    vok!(vnl!())
 }
