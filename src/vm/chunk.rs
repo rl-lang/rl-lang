e59e75cd76e4ab97dde4cbcc2235469133f60ae7
MohamedGonem <73464078+MohamedGonem@users.noreply.github.com>
2026-07-11 11:49:10 +0300

    add representation of each opcode

--- src/vm/chunk.rs
+++ src/vm/chunk.rs
@@ -1,23 +1,41 @@
 #[repr(u8)]
 #[derive(Debug, Clone, Copy, PartialEq)]
 pub enum OpCode {
+    /// literal
     Const = 0,
+    /// +
     Add = 1,
+    /// -
     Sub = 2,
+    /// *
     Mul = 3,
+    /// /
     Div = 4,
+    /// unary -
     Negate = 5,
+    /// !
     Not = 6,
+    /// ==
     Eq = 7,
+    /// !=
     NotEq = 8,
+    /// <
     Less = 9,
+    /// <=
     LessEq = 10,
+    /// >
     Greater = 11,
+    /// >=
     GreaterEq = 12,
+    /// read value
     GetLocal = 13,
+    /// reassign value
     SetLocal = 14,
+    /// dec / CONST
     DefineLocal = 15,
+    /// discard
     Pop = 16,
+    /// end of program
     Return = 17,
 }
 
