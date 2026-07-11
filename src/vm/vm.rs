7661e98dd449aeee5387c445a5bb8d7112dcebee
MohamedGonem <73464078+MohamedGonem@users.noreply.github.com>
2026-07-11 12:46:02 +0300

    add helper functions for math operations

--- src/vm/vm.rs
+++ src/vm/vm.rs
@@ -29,6 +29,65 @@ impl Vm {
         Ok((a, b))
     }
 
+    /// Helper function for arth operations
+    /// handles +, -, *
+    /// currently promotes int to float
+    fn binary_numeric(
+        &mut self,
+        int_op: fn(i64, i64) -> i64,
+        float_op: fn(f64, f64) -> f64,
+    ) -> Result<(), VmError> {
+        let (a, b) = self.pop_two()?;
+        let out = match (a, b) {
+            (VmValue::Int(a), VmValue::Int(b)) => VmValue::Int(int_op(a, b)),
+            (VmValue::Float(a), VmValue::Float(b)) => VmValue::Float(float_op(a, b)),
+            // for now int promoted to float
+            // should disable later after wiring `as` Cast keyword
+            (VmValue::Int(a), VmValue::Float(b)) => VmValue::Float(float_op(a as f64, b)),
+            (VmValue::Float(a), VmValue::Int(b)) => VmValue::Float(float_op(a, b as f64)),
+            (a, b) => {
+                return Err(VmError(format!(
+                    "cannot apply arithmetic op to {a:?} and {b:?}"
+                )));
+            }
+        };
+        self.stack.push(out);
+        Ok(())
+    }
+
+    /// Helper function for arth operations
+    /// handles /
+    /// currently promotes int to float
+    fn binary_div(&mut self) -> Result<(), VmError> {
+        let (a, b) = self.pop_two()?;
+        let out = match (a, b) {
+            (VmValue::Int(_), VmValue::Int(0)) => return Err(VmError("division by zero".into())),
+            (VmValue::Int(a), VmValue::Int(b)) => VmValue::Int(a / b),
+            (VmValue::Float(a), VmValue::Float(b)) => VmValue::Float(a / b),
+            (VmValue::Int(a), VmValue::Float(b)) => VmValue::Float(a as f64 / b),
+            (VmValue::Float(a), VmValue::Int(b)) => VmValue::Float(a / b as f64),
+            (a, b) => return Err(VmError(format!("cannot divide {a:?} by {b:?}"))),
+        };
+        self.stack.push(out);
+        Ok(())
+    }
+
+    /// Helper function for comparsion operations
+    /// accepts float/float, int/int and promotes int to float
+    /// handles >, <, >=, <=
+    fn binary_cmp(&mut self, pred: fn(std::cmp::Ordering) -> bool) -> Result<(), VmError> {
+        let (a, b) = self.pop_two()?;
+        let ord = match (&a, &b) {
+            (VmValue::Int(a), VmValue::Int(b)) => a.partial_cmp(b),
+            (VmValue::Float(a), VmValue::Float(b)) => a.partial_cmp(b),
+            (VmValue::Int(a), VmValue::Float(b)) => (*a as f64).partial_cmp(b),
+            (VmValue::Float(a), VmValue::Int(b)) => a.partial_cmp(&(*b as f64)),
+            _ => return Err(VmError(format!("cannot compare {a:?} and {b:?}"))),
+        }
+        .ok_or_else(|| VmError("comparison produced no ordering (NaN?)".into()))?;
+        self.stack.push(VmValue::Bool(pred(ord)));
+        Ok(())
+    }
 }
 
 impl Default for Vm {
