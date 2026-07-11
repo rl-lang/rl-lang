6c7a0d1688649584274bb8ccf611372c0487482c
theroguevigilante <naimish82@proton.me>
2026-07-11 11:29:46 +0530

    Refactor rand_int_range

--- src/interpreter/stdlib/random/rand_int_range.rs
+++ src/interpreter/stdlib/random/rand_int_range.rs
@@ -1,15 +1,15 @@
 use crate::{
-    interpreter::{evaluator::Evaluator, values::Value},
-    utils::{errors::Error, span::Span},
+    interpreter::evaluator::Evaluator,
+    interpreter::stdlib::common::{verr, vi, vok, vs},
+    interpreter::values::Value,
 };
 
-pub fn func(eval: &mut Evaluator, min: i64, max: i64, span: Span) -> Result<Value, Error> {
+pub fn func(eval: &mut Evaluator, min: i64, max: i64) -> Value {
     if min >= max {
-        return Err(eval.err(
-            "min value shouldn't be bigger than or equal to maximum value",
-            span,
+        return verr!(vs!(
+            "min value shouldn't be bigger than or equal to maximum value".to_string()
         ));
     }
 
-    Ok(Value::Integer(eval.rng.generate_random_int_range(min, max)))
+    vok!(vi!(eval.rng.generate_random_int_range(min, max)))
 }
