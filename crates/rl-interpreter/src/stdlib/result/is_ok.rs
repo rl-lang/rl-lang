9dcba0eb3b9668681f3e79f5b2bb95596080bfbe
Mohamed Gonem <73464078+MohamedGonem@users.noreply.github.com>
2026-07-13 21:34:20 +0300

    change `is_ok()` to return `bool` instead of `result[bool]`

--- crates/rl-interpreter/src/stdlib/result/is_ok.rs
+++ crates/rl-interpreter/src/stdlib/result/is_ok.rs
@@ -2,6 +2,6 @@ use crate::evaluator::Evaluator;
 use crate::stdlib::common::{vb, vok};
 use crate::values::Value;
 
-pub fn func(_: &mut Evaluator, value: Value) -> Value {
-    vok!(vb!(value.is_ok()))
+pub fn func(_: &mut Evaluator, value: Value) -> bool {
+    value.is_ok()
 }
