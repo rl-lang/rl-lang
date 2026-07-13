168632aa02003b945bc2f0b23c4b5682f10d66d5
MohamedGonem <73464078+MohamedGonem@users.noreply.github.com>
2026-07-13 09:49:23 +0300

    update count_bits

--- crates/rl-interpreter/src/stdlib/bitwise/count_bits.rs
+++ crates/rl-interpreter/src/stdlib/bitwise/count_bits.rs
@@ -1,17 +1,13 @@
-use crate::{evaluator::Evaluator, values::Value};
-use rl_utils::{errors::Error, span::Span};
+use crate::{
+    evaluator::Evaluator,
+    stdlib::common::{vby, verr, vi, vok, vs},
+    values::Value,
+};
 
-pub fn std_count_bits(eval: &mut Evaluator, args: Vec<Value>, span: Span) -> Result<Value, Error> {
-    if args.len() != 1 {
-        return Err(eval.err(
-            format!("count_bits() expects 1 argument, got {}", args.len()),
-            span,
-        ));
-    }
-
-    match args.into_iter().next().unwrap_or(Value::Null) {
-        Value::Byte(x) => Ok(Value::Byte(x.count_ones() as u8)),
-        Value::Integer(x) => Ok(Value::Integer(x.count_ones() as i64)),
-        _ => Err(eval.err("count_bits() expects a byte or an int".to_string(), span)),
+pub fn std_count_bits(_: &mut Evaluator, v: Value) -> Value {
+    match v {
+        Value::Byte(x) => vok!(vby!(x.count_ones() as u8)),
+        Value::Integer(x) => vok!(vi!(x.count_ones() as i64)),
+        _ => verr!(vs!("count_bits expects a byte or an int".to_string())),
     }
 }
