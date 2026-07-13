addd6b6d8baee6f98c8866c24b4c3fd431b8c4a2
MohamedGonem <73464078+MohamedGonem@users.noreply.github.com>
2026-07-13 07:48:40 +0300

    update exit function and remove useless Result

--- crates/rl-interpreter/src/stdlib/process/exit.rs
+++ crates/rl-interpreter/src/stdlib/process/exit.rs
@@ -1,6 +1,5 @@
 use crate::{evaluator::Evaluator, values::Value};
-use rl_utils::{errors::Error, span::Span};
 
-pub fn std_exit(_: &mut Evaluator, code: i64, _: Span) -> Result<Value, Error> {
+pub fn std_exit(_: &mut Evaluator, code: i64) -> Value {
     std::process::exit(code as i32);
 }
