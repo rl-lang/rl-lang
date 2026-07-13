9bd06c41cf212d98ffded6206f591ef7c9389721
MohamedGonem <73464078+MohamedGonem@users.noreply.github.com>
2026-07-13 08:33:36 +0300

    change floor to return result instead

--- crates/rl-interpreter/src/stdlib/math/floor.rs
+++ crates/rl-interpreter/src/stdlib/math/floor.rs
@@ -1,13 +1,16 @@
-use crate::{evaluator::Evaluator, values::Value};
-use rl_utils::{errors::Error, span::Span};
+use crate::{
+    evaluator::Evaluator,
+    stdlib::common::{verr, vf, vi, vok, vs},
+    values::Value,
+};
 
-pub fn std_floor(eval: &mut Evaluator, a: Value, span: Span) -> Result<Value, Error> {
+pub fn std_floor(_: &mut Evaluator, a: Value) -> Value {
     match a {
-        Value::Integer(i) => Ok(Value::Integer(i)),
-        Value::Float(f) => Ok(Value::Float(f.floor())),
-        other => Err(eval.err(
-            format!("floor() expects a number, got {}", other.type_name()),
-            span,
-        )),
+        Value::Integer(i) => vok!(vi!(i)),
+        Value::Float(f) => vok!(vf!(f.floor())),
+        other => verr!(vs!(format!(
+            "floor expects a number, got {}",
+            other.type_name()
+        ))),
     }
 }
