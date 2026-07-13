0202325f698674e13005b112d5d3e5d351a38f63
MohamedGonem <73464078+MohamedGonem@users.noreply.github.com>
2026-07-13 08:44:13 +0300

    change modulo to return result instead and remove implict conversion between byte and int

--- crates/rl-interpreter/src/stdlib/math/modulo.rs
+++ crates/rl-interpreter/src/stdlib/math/modulo.rs
@@ -1,29 +1,19 @@
-use crate::{evaluator::Evaluator, values::Value};
-use rl_utils::{errors::Error, span::Span};
+use crate::{
+    evaluator::Evaluator,
+    stdlib::common::{verr, vf, vi, vok, vs},
+    values::Value,
+};
 
-pub fn std_mod(eval: &mut Evaluator, a: Value, b: Value, span: Span) -> Result<Value, Error> {
-    let a = normalize_numeric(a);
-    let b = normalize_numeric(b);
-
+pub fn std_mod(_: &mut Evaluator, a: Value, b: Value) -> Value {
     match (a, b) {
-        (Value::Integer(a), Value::Integer(b)) => Ok(Value::Integer(a % b)),
-        (Value::Float(a), Value::Float(b)) => Ok(Value::Float(a % b)),
-        (Value::Integer(a), Value::Float(b)) => Ok(Value::Float(a as f64 % b)),
-        (Value::Float(a), Value::Integer(b)) => Ok(Value::Float(a % b as f64)),
-        (a, b) => Err(eval.err(
-            format!(
-                "mod() expects a number, got ({}, {})",
-                a.type_name(),
-                b.type_name()
-            ),
-            span,
-        )),
-    }
-}
-
-fn normalize_numeric(v: Value) -> Value {
-    match v {
-        Value::Byte(b) => Value::Integer(b as i64),
-        other => other,
+        (Value::Integer(a), Value::Integer(b)) => vok!(vi!(a % b)),
+        (Value::Float(a), Value::Float(b)) => vok!(vf!(a % b)),
+        (Value::Integer(a), Value::Float(b)) => vok!(vf!(a as f64 % b)),
+        (Value::Float(a), Value::Integer(b)) => vok!(vf!(a % b as f64)),
+        (a, b) => verr!(vs!(format!(
+            "mod expects a number, got ({}, {})",
+            a.type_name(),
+            b.type_name()
+        ))),
     }
 }
