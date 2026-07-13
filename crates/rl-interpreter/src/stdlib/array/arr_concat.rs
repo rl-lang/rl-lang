5824defa4dc1d674c17e5dbe3cff132b57842d57
MohamedGonem <73464078+MohamedGonem@users.noreply.github.com>
2026-07-13 11:54:24 +0300

    update arr_concat

--- crates/rl-interpreter/src/stdlib/array/arr_concat.rs
+++ crates/rl-interpreter/src/stdlib/array/arr_concat.rs
@@ -1,12 +1,10 @@
-use crate::{evaluator::Evaluator, values::Value};
-use rl_utils::{errors::Error, span::Span};
+use crate::{
+    evaluator::Evaluator,
+    stdlib::common::{verr, vok, vs},
+    values::Value,
+};
 
-pub fn std_arr_concat(
-    eval: &mut Evaluator,
-    array1: Value,
-    array2: Value,
-    span: Span,
-) -> Result<Value, Error> {
+pub fn std_arr_concat(_: &mut Evaluator, array1: Value, array2: Value) -> Value {
     match (array1, array2) {
         (
             Value::Values {
@@ -19,21 +17,18 @@ pub fn std_arr_concat(
             },
         ) => {
             if it_1 != it_2 {
-                return Err(eval.err(
-                    format!(
-                        "type mismatch: array type {:?}, cannot concat {:?}",
-                        it_1, it_2
-                    ),
-                    span,
-                ));
+                return verr!(vs!(format!(
+                    "arr_concat: type mismatch: array type {:?}, cannot concat {:?}",
+                    it_1, it_2
+                )));
             }
             let mut v = i1;
             v.extend(i2);
-            Ok(Value::Values {
+            vok!(Value::Values {
                 items_type: it_1,
-                items: v,
+                items: v
             })
         }
-        _ => Err(eval.err("arr_concat() accepts only arrays".to_string(), span)),
+        _ => verr!(vs!("arr_concat: accepts only arrays".to_string())),
     }
 }
