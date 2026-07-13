67ef57a0545f9048e54d109c04a42fbb8bd4b3d1
Mohamed Gonem <73464078+MohamedGonem@users.noreply.github.com>
2026-07-14 01:30:22 +0300

    Update result_map.rs

--- crates/rl-interpreter/src/stdlib/result/result_map.rs
+++ crates/rl-interpreter/src/stdlib/result/result_map.rs
@@ -3,40 +3,50 @@ use crate::{
     stdlib::common::{verr, vok, vs},
     values::Value,
 };
-use rl_utils::span::Span;
+use rl_utils::{errors::Error, span::Span};
 
-pub fn std_result_map(eval: &mut Evaluator, a: Value, b: Value, span: Span) -> Value {
+pub fn std_result_map(
+    eval: &mut Evaluator,
+    a: Value,
+    b: Value,
+    span: Span,
+) -> Result<Value, Error> {
     match a {
         Value::Ok(inner) => {
             let mapped = match eval.call_value(b, vec![*inner], span) {
                 Ok(mapped) => mapped,
-                Err(e) => return verr!(vs!(e.message().to_string())),
+                Err(e) => return Ok(verr!(vs!(e.message().to_string()))),
             };
-            vok!(mapped)
+            Ok(vok!(mapped))
         }
         // pass error as is
-        Value::Err(_) => a,
-        other => verr!(vs!(format!(
+        Value::Err(_) => Ok(a),
+        other => Ok(verr!(vs!(format!(
             "result_map: expected result, got {}",
             other.type_name()
-        ))),
+        )))),
     }
 }
 
-pub fn std_result_map_err(eval: &mut Evaluator, a: Value, b: Value, span: Span) -> Value {
+pub fn std_result_map_err(
+    eval: &mut Evaluator,
+    a: Value,
+    b: Value,
+    span: Span,
+) -> Result<Value, Error> {
     match a {
         Value::Err(inner) => {
             let mapped = match eval.call_value(b, vec![*inner], span) {
                 Ok(mapped) => mapped,
-                Err(e) => return verr!(vs!(e.message().to_string())),
+                Err(e) => return Ok(verr!(vs!(e.message().to_string()))),
             };
-            verr!(mapped)
+            Ok(verr!(mapped))
         }
         // pass ok as is
-        Value::Ok(_) => a,
-        other => verr!(vs!(format!(
+        Value::Ok(_) => Ok(a),
+        other => Ok(verr!(vs!(format!(
             "result_map_err: expected result, got {}",
             other.type_name()
-        ))),
+        )))),
     }
 }
