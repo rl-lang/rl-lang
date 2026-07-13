d02d09aa41a46da25a8bd7e6794fe0ccfd5677ab
MohamedGonem <73464078+MohamedGonem@users.noreply.github.com>
2026-07-13 11:56:26 +0300

    update arr_is_empty

--- crates/rl-interpreter/src/stdlib/array/arr_is_empty.rs
+++ crates/rl-interpreter/src/stdlib/array/arr_is_empty.rs
@@ -1,14 +1,15 @@
-use crate::{evaluator::Evaluator, values::Value};
-use rl_utils::{errors::Error, span::Span};
+use crate::{
+    evaluator::Evaluator,
+    stdlib::common::{verr, vok, vs},
+    values::Value,
+};
 
-pub fn std_arr_is_empty(eval: &mut Evaluator, array: Value, span: Span) -> Result<bool, Error> {
+pub fn std_arr_is_empty(_: &mut Evaluator, array: Value) -> Value {
     match array {
-        Value::Values { items, .. } => {
-            if items.is_empty() {
-                return Ok(true);
-            }
-            Ok(false)
-        }
-        _ => Err(eval.err("arr_is_empty() accepts only arrays".to_string(), span)),
+        Value::Values { items, .. } => vok!(Value::Bool(items.is_empty())),
+        other => verr!(vs!(format!(
+            "arr_is_empty: accepts only arrays, found {}",
+            other.type_name()
+        ))),
     }
 }
