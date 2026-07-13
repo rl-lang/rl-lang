67d75afd358ba000512806057ce7c403f11f3ec1
MohamedGonem <73464078+MohamedGonem@users.noreply.github.com>
2026-07-13 10:30:16 +0300

    update read_lines

--- crates/rl-interpreter/src/stdlib/io/read_lines.rs
+++ crates/rl-interpreter/src/stdlib/io/read_lines.rs
@@ -1,21 +1,26 @@
-use crate::{evaluator::Evaluator, values::Value};
+use crate::{
+    evaluator::Evaluator,
+    stdlib::common::{verr, vok, vs},
+    values::Value,
+};
 use rl_ast::statements::TypeAnnotation;
-use rl_utils::{errors::Error, span::Span};
 
-pub fn std_read_lines(eval: &mut Evaluator, file: String, span: Span) -> Result<Value, Error> {
-    let data = std::fs::read_to_string(&file)
-        .map_err(|e| {
-            eval.err(
-                format!("read_lines(): failed to read \"{}\": {}", file, e),
-                span,
-            )
-        })?
-        .lines()
-        .map(String::from)
-        .map(Value::String)
-        .collect::<Vec<Value>>();
+pub fn std_read_lines(_: &mut Evaluator, file: String) -> Value {
+    let data = match std::fs::read_to_string(&file) {
+        Err(e) => {
+            return verr!(vs!(format!(
+                "read_lines: failed to read \"{}\": {}",
+                file, e
+            )));
+        }
+        Ok(d) => d
+            .lines()
+            .map(String::from)
+            .map(Value::String)
+            .collect::<Vec<Value>>(),
+    };
 
-    Ok(Value::Values {
+    vok!(Value::Values {
         items_type: TypeAnnotation::String,
         items: data,
     })
