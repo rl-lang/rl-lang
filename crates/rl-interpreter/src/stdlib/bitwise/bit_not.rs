3a2365767003b0426cfacc3bc40b0c4c6dfe6b9e
MohamedGonem <73464078+MohamedGonem@users.noreply.github.com>
2026-07-13 09:49:04 +0300

    update bit_not

--- crates/rl-interpreter/src/stdlib/bitwise/bit_not.rs
+++ crates/rl-interpreter/src/stdlib/bitwise/bit_not.rs
@@ -1,17 +1,13 @@
-use crate::{evaluator::Evaluator, values::Value};
-use rl_utils::{errors::Error, span::Span};
+use crate::{
+    evaluator::Evaluator,
+    stdlib::common::{vby, verr, vi, vok, vs},
+    values::Value,
+};
 
-pub fn std_bit_not(eval: &mut Evaluator, args: Vec<Value>, span: Span) -> Result<Value, Error> {
-    if args.len() != 1 {
-        return Err(eval.err(
-            format!("bit_not() expects 1 argument, got {}", args.len()),
-            span,
-        ));
-    }
-
-    match args.into_iter().next().unwrap_or(Value::Null) {
-        Value::Byte(x) => Ok(Value::Byte(!x)),
-        Value::Integer(x) => Ok(Value::Integer(!x)),
-        _ => Err(eval.err("bit_not() expects a byte or an int".to_string(), span)),
+pub fn std_bit_not(_: &mut Evaluator, v: Value) -> Value {
+    match v {
+        Value::Byte(x) => vok!(vby!(!x)),
+        Value::Integer(x) => vok!(vi!(!x)),
+        _ => verr!(vs!("bit_not expects a byte or an int".to_string())),
     }
 }
