edbca8abdff01d43d3eb4dc952cd7222dd311f02
MohamedGonem <73464078+MohamedGonem@users.noreply.github.com>
2026-07-13 09:49:02 +0300

    update bit_and

--- crates/rl-interpreter/src/stdlib/bitwise/bit_and.rs
+++ crates/rl-interpreter/src/stdlib/bitwise/bit_and.rs
@@ -1,26 +1,15 @@
-use crate::{evaluator::Evaluator, values::Value};
-use rl_utils::{errors::Error, span::Span};
+use crate::{
+    evaluator::Evaluator,
+    stdlib::common::{vby, verr, vi, vok, vs},
+    values::Value,
+};
 
-pub fn std_bit_and(eval: &mut Evaluator, args: Vec<Value>, span: Span) -> Result<Value, Error> {
-    if args.len() != 2 {
-        return Err(eval.err(
-            format!("bit_and() expects 2 arguments, got {}", args.len()),
-            span,
-        ));
-    }
-
-    let mut iter = args.into_iter();
-    let a = iter.next().unwrap();
-    let b = iter.next().unwrap();
-
+pub fn std_bit_and(_: &mut Evaluator, a: Value, b: Value) -> Value {
     match (a, b) {
-        (Value::Byte(x), Value::Byte(y)) => Ok(Value::Byte(x & y)),
-        (Value::Integer(x), Value::Integer(y)) => Ok(Value::Integer(x & y)),
-        (Value::Byte(x), Value::Integer(y)) => Ok(Value::Integer(x as i64 & y)),
-        (Value::Integer(x), Value::Byte(y)) => Ok(Value::Integer(x & y as i64)),
-        _ => Err(eval.err(
-            "bit_and() expects byte or integer arguments".to_string(),
-            span,
-        )),
+        (Value::Byte(x), Value::Byte(y)) => vok!(vby!(x & y)),
+        (Value::Integer(x), Value::Integer(y)) => vok!(vi!(x & y)),
+        (Value::Byte(x), Value::Integer(y)) => vok!(vi!(x as i64 & y)),
+        (Value::Integer(x), Value::Byte(y)) => vok!(vi!(x & y as i64)),
+        _ => verr!(vs!("bit_and expects byte or integer arguments".to_string())),
     }
 }
