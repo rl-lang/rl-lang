285e187e460f9288cdbf6c65eee4cd91640fb768
MohamedGonem <73464078+MohamedGonem@users.noreply.github.com>
2026-07-13 10:29:57 +0300

    update delete_file

--- crates/rl-interpreter/src/stdlib/io/delete_file.rs
+++ crates/rl-interpreter/src/stdlib/io/delete_file.rs
@@ -1,12 +1,15 @@
-use crate::{evaluator::Evaluator, values::Value};
-use rl_utils::{errors::Error, span::Span};
+use crate::{
+    evaluator::Evaluator,
+    stdlib::common::{verr, vnl, vok, vs},
+    values::Value,
+};
 
-pub fn std_delete_file(eval: &mut Evaluator, file: String, span: Span) -> Result<Value, Error> {
-    std::fs::remove_file(&file).map_err(|e| {
-        eval.err(
-            format!("delete_file(): failed to read \"{}\": {}", file, e),
-            span,
-        )
-    })?;
-    Ok(Value::Null)
+pub fn std_delete_file(_: &mut Evaluator, file: String) -> Value {
+    match std::fs::remove_file(&file) {
+        Ok(_) => vok!(vnl!()),
+        Err(e) => verr!(vs!(format!(
+            "delete_file: failed to read \"{}\": {}",
+            file, e
+        ))),
+    }
 }
