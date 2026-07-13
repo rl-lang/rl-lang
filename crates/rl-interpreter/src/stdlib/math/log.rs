eb018955f522cc66b925c4ef945eb856f7bb4661
MohamedGonem <73464078+MohamedGonem@users.noreply.github.com>
2026-07-13 08:50:03 +0300

    change log functions to return result instead

--- crates/rl-interpreter/src/stdlib/math/log.rs
+++ crates/rl-interpreter/src/stdlib/math/log.rs
@@ -1,20 +1,20 @@
-use crate::{evaluator::Evaluator, values::Value};
-use rl_utils::{errors::Error, span::Span};
+use crate::{
+    evaluator::Evaluator,
+    stdlib::common::{verr, vf, vok, vs},
+    values::Value,
+};
 
-pub fn std_log(eval: &mut Evaluator, a: Value, base: Value, span: Span) -> Result<Value, Error> {
+pub fn std_log(_: &mut Evaluator, a: Value, base: Value) -> Value {
     match (a, base) {
-        (Value::Integer(i), Value::Integer(base)) => Ok(Value::Float((i as f64).log(base as f64))),
-        (Value::Float(f), Value::Float(base)) => Ok(Value::Float(f.log(base))),
-        (Value::Float(f), Value::Integer(base)) => Ok(Value::Float(f.log(base as f64))),
-        (Value::Integer(i), Value::Float(base)) => Ok(Value::Float((i as f64).log(base))),
+        (Value::Integer(i), Value::Integer(base)) => vok!(vf!((i as f64).log(base as f64))),
+        (Value::Float(f), Value::Float(base)) => vok!(vf!(f.log(base))),
+        (Value::Float(f), Value::Integer(base)) => vok!(vf!(f.log(base as f64))),
+        (Value::Integer(i), Value::Float(base)) => vok!(vf!((i as f64).log(base))),
 
-        (a, base) => Err(eval.err(
-            format!(
-                "log() expects a number, got ({}, {})",
-                a.type_name(),
-                base.type_name()
-            ),
-            span,
-        )),
+        (a, base) => verr!(vs!(format!(
+            "log expects a number, got ({}, {})",
+            a.type_name(),
+            base.type_name()
+        ))),
     }
 }
