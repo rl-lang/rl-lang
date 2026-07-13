3da469c0e3a58ee7b15aaa0758d8b34ec2bbe62b
MohamedGonem <73464078+MohamedGonem@users.noreply.github.com>
2026-07-13 09:49:48 +0300

    update trailing_zeros

--- crates/rl-interpreter/src/stdlib/bitwise/trailing_zeros.rs
+++ crates/rl-interpreter/src/stdlib/bitwise/trailing_zeros.rs
@@ -1,24 +1,13 @@
-use crate::{evaluator::Evaluator, values::Value};
-use rl_utils::{errors::Error, span::Span};
+use crate::{
+    evaluator::Evaluator,
+    stdlib::common::{verr, vok, vs},
+    values::Value,
+};
 
-pub fn std_trailing_zeros(
-    eval: &mut Evaluator,
-    args: Vec<Value>,
-    span: Span,
-) -> Result<Value, Error> {
-    if args.len() != 1 {
-        return Err(eval.err(
-            format!("trailing_zeros() expects 1 argument, got {}", args.len()),
-            span,
-        ));
-    }
-
-    match args.into_iter().next().unwrap_or(Value::Null) {
-        Value::Byte(x) => Ok(Value::Byte(u8::trailing_zeros(x) as u8)),
-        Value::Integer(x) => Ok(Value::Integer(i64::trailing_zeros(x) as i64)),
-        _ => Err(eval.err(
-            "trailing_zeros() expects a byte or an int".to_string(),
-            span,
-        )),
+pub fn std_trailing_zeros(_: &mut Evaluator, v: Value) -> Value {
+    match v {
+        Value::Byte(x) => vok!(Value::Byte(u8::trailing_zeros(x) as u8)),
+        Value::Integer(x) => vok!(Value::Integer(i64::trailing_zeros(x) as i64)),
+        _ => verr!(vs!("trailing_zeros expects a byte or an int".to_string())),
     }
 }
