e05823187429927eac655e79ef7b6996c91bc006
MohamedGonem <73464078+MohamedGonem@users.noreply.github.com>
2026-07-13 11:31:44 +0300

    update path_is_dir and path_is_file

--- crates/rl-interpreter/src/stdlib/path/path_is_file.rs
+++ crates/rl-interpreter/src/stdlib/path/path_is_file.rs
@@ -1,12 +1,15 @@
-use crate::{evaluator::Evaluator, values::Value};
-use rl_utils::{errors::Error, span::Span};
+use crate::{
+    evaluator::Evaluator,
+    stdlib::common::{verr, vok, vs},
+    values::Value,
+};
 
-pub fn std_path_is_file(eval: &mut Evaluator, path: Value, span: Span) -> Result<Value, Error> {
+pub fn std_path_is_file(_: &mut Evaluator, path: Value) -> Value {
     match path {
-        Value::String(s) => Ok(Value::Bool(std::path::Path::new(&s).is_file())),
-        other => Err(eval.err(
-            format!("path_is_file() expects a string, got {}", other.type_name()),
-            span,
-        )),
+        Value::String(s) => vok!(Value::Bool(std::path::Path::new(&s).is_file())),
+        other => verr!(vs!(format!(
+            "path_is_file: expects a string, got {}",
+            other.type_name()
+        ))),
     }
 }
