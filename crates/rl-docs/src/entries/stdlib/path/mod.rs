72d14138594ce63ef832174a9d8a02fae71f4dc2
MohamedGonem <73464078+MohamedGonem@users.noreply.github.com>
2026-07-16 12:45:21 +0300

    change `path_is_file` to return `bool` directly and update docs

--- crates/rl-docs/src/entries/stdlib/path/mod.rs
+++ crates/rl-docs/src/entries/stdlib/path/mod.rs
@@ -78,15 +78,18 @@ path_is_dir("./src")"#,
     since: Some("v0.1.5"),
 };
 
-static PATH_IS_FILE: FnEntry = FnEntry {
+pub static PATH_IS_FILE: FnEntry = FnEntry {
     signature: "path_is_file(path)",
     description: "returns true if the path is a file",
-    example: "get std::path::path_is_file\n\npath_is_file(\"./Cargo.toml\") // true",
-    expected_output: None,
-    returns: "",
+    example: r#"
+get std::path::path_is_file
+
+path_is_file("./Cargo.toml")"#,
+    expected_output: Some("true"),
+    returns: "bool",
     errors: None,
     see_also: &[],
-    since: None,
+    since: Some("v0.1.5"),
 };
 
 static PATH_JOIN: FnEntry = FnEntry {
