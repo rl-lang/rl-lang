9bdc85fc558d92151dc578a8fd2fc0914f8ce47f
MohamedGonem <73464078+MohamedGonem@users.noreply.github.com>
2026-07-17 19:52:57 +0300

    update `time_parts` docs entry

--- crates/rl-docs/src/entries/stdlib/time/time_parts.rs
+++ crates/rl-docs/src/entries/stdlib/time/time_parts.rs
@@ -3,10 +3,12 @@ use crate::entry::FnEntry;
 pub static TIME_PARTS: FnEntry = FnEntry {
     signature: "time_parts(timestamp)",
     description: "returns an array of [year, month, day, hour, minute, second] for the given unix timestamp (UTC)",
-    example: "get std::time::time_parts\n\ntime_parts(1719000000) // [2024, 6, 21, 20, 0, 0]",
-    expected_output: None,
-    returns: "",
-    errors: None,
+    example: r#"get std::time::time_parts
+
+time_parts(1784306758)?"#,
+    expected_output: Some("[2026, 7, 17, 16, 46, 0]"),
+    returns: "result[arr[int]]",
+    errors: Some("Will return error on negative timestamp"),
     see_also: &[],
     since: Some("v0.1.5"),
 };
