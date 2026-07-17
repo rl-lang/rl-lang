0facffbd1d4caab40d029c1312e28d2ac6c6162f
MohamedGonem <73464078+MohamedGonem@users.noreply.github.com>
2026-07-17 18:30:15 +0300

    fix newline in `path` docs

--- crates/rl-docs/src/entries/stdlib/path/path_is_file.rs
+++ crates/rl-docs/src/entries/stdlib/path/path_is_file.rs
@@ -3,8 +3,7 @@ use crate::entry::FnEntry;
 pub static PATH_IS_FILE: FnEntry = FnEntry {
     signature: "path_is_file(path)",
     description: "returns true if the path is a file",
-    example: r#"
-get std::path::path_is_file
+    example: r#"get std::path::path_is_file
 
 path_is_file("./Cargo.toml")"#,
     expected_output: Some("true"),
--- crates/rl-docs/src/entries/stdlib/path/path_pop.rs
+++ crates/rl-docs/src/entries/stdlib/path/path_pop.rs
@@ -3,8 +3,7 @@ use crate::entry::FnEntry;
 pub static PATH_POP: FnEntry = FnEntry {
     signature: "path_pop(path)",
     description: "removes the last component of the path and returns the result",
-    example: r#"
-get std::path::path_pop
+    example: r#"get std::path::path_pop
 
 path_pop("/usr/bin/rl")"#,
     expected_output: Some("/usr/bin"),
