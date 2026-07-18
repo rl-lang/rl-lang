ec058323e4a8ac38c8cc44ee41eb98b7d4502306
MohamedGonem <73464078+MohamedGonem@users.noreply.github.com>
2026-07-18 17:41:41 +0300

    fix `arr_flatten` from erroring on empty array

--- crates/rl-interpreter/src/stdlib/array/arr_flatten.rs
+++ crates/rl-interpreter/src/stdlib/array/arr_flatten.rs
@@ -6,24 +6,19 @@ use crate::{
 
 pub fn std_arr_flatten(_: &mut Evaluator, array: Value) -> Value {
     match array {
-        Value::Values { items, items_type } => {
-            if items.is_empty() {
-                return verr!(vs!("arr_flatten: called on empty array".to_string()));
-            }
-            vok!(Value::Values {
-                items_type,
-                items: items
-                    .into_iter()
-                    .flat_map(|v| {
-                        if let Value::Values { items, .. } = v {
-                            items
-                        } else {
-                            vec![v]
-                        }
-                    })
-                    .collect(),
-            })
-        }
+        Value::Values { items, items_type } => vok!(Value::Values {
+            items_type,
+            items: items
+                .into_iter()
+                .flat_map(|v| {
+                    if let Value::Values { items, .. } = v {
+                        items
+                    } else {
+                        vec![v]
+                    }
+                })
+                .collect(),
+        }),
         other => verr!(vs!(format!(
             "arr_flatten: accepts only arrays, found {}",
             other.type_name()
